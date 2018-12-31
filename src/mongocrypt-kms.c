eb3f348a40fbea8ce79dd648017d0d10b7ea5602
Kevin Albertson <kevin.albertson@mongodb.com>
2018-12-31 14:40:02 -0500

    formatting + optional tracing

--- src/mongocrypt-kms.c
+++ src/mongocrypt-kms.c
@@ -14,252 +14,235 @@
  * limitations under the License.
  */
 
-#include "mongoc/mongoc.h"
-#include "mongocrypt-private.h"
 #include "kms_message/kms_b64.h"
 #include "kms_message/kms_message.h"
+#include "mongoc/mongoc.h"
+#include "mongocrypt-private.h"
 
-#define ERRNO_IS_AGAIN(errno)                                          \
-   ((errno == EINTR) || (errno == EAGAIN) || (errno == EWOULDBLOCK) || \
-    (errno == EINPROGRESS))
+#define ERRNO_IS_AGAIN(errno)                                                  \
+  ((errno == EINTR) || (errno == EAGAIN) || (errno == EWOULDBLOCK) ||          \
+   (errno == EINPROGRESS))
 
+static mongoc_stream_t *_get_aws_stream(mongoc_crypt_error_t *error) {
+  int errcode;
+  int r;
+  struct sockaddr_in server_addr = {0};
+  mongoc_socket_t *conn_sock = NULL;
+  mongoc_stream_t *stream = NULL;
+  mongoc_stream_t *tls_stream = NULL;
+  mongoc_ssl_opt_t ssl_opts = {0};
+  bson_error_t bson_error;
 
-static mongoc_stream_t *
-_get_aws_stream (mongoc_crypt_error_t *error)
-{
-   int errcode;
-   int r;
-   struct sockaddr_in server_addr = {0};
-   mongoc_socket_t *conn_sock = NULL;
-   mongoc_stream_t *stream = NULL;
-   mongoc_stream_t *tls_stream = NULL;
-   mongoc_ssl_opt_t ssl_opts = {0};
-   bson_error_t bson_error;
+  memcpy(&ssl_opts, mongoc_ssl_opt_get_default(), sizeof ssl_opts);
+  conn_sock = mongoc_socket_new(AF_INET, SOCK_STREAM, 0);
+  if (!conn_sock) {
+    SET_CRYPT_ERR("could not create socket to AWS");
+    return NULL;
+  }
 
-   memcpy (&ssl_opts, mongoc_ssl_opt_get_default (), sizeof ssl_opts);
-   conn_sock = mongoc_socket_new (AF_INET, SOCK_STREAM, 0);
-   if (!conn_sock) {
-      SET_CRYPT_ERR ("could not create socket to AWS");
-      return NULL;
-   }
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(443);
+  /* TODO: actually do a DNS lookup. */
+  /* 54.239.18.135, kms.us-east-1.amazonaws.com */
+  server_addr.sin_addr.s_addr = htonl(0x36EF1287);
+  r = mongoc_socket_connect(conn_sock, (struct sockaddr *)&server_addr,
+                            sizeof(server_addr), -1);
 
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_port = htons (443);
-   /* TODO: actually do a DNS lookup. */
-   /* 54.239.18.135, kms.us-east-1.amazonaws.com */
-   server_addr.sin_addr.s_addr = htonl (0x36EF1287);
-   r = mongoc_socket_connect (
-      conn_sock, (struct sockaddr *) &server_addr, sizeof (server_addr), -1);
+  errcode = mongoc_socket_errno(conn_sock);
+  if (!(r == 0 || ERRNO_IS_AGAIN(errcode))) {
+    mongoc_socket_destroy(conn_sock);
+    SET_CRYPT_ERR("mongoc_socket_connect unexpected return: %d (errno: %d)\n",
+                  r, errcode);
+    return NULL;
+  }
 
-   errcode = mongoc_socket_errno (conn_sock);
-   if (!(r == 0 || ERRNO_IS_AGAIN (errcode))) {
-      mongoc_socket_destroy (conn_sock);
-      SET_CRYPT_ERR (
-         "mongoc_socket_connect unexpected return: %d (errno: %d)\n",
-         r,
-         errcode);
-      return NULL;
-   }
-
-   stream = mongoc_stream_socket_new (conn_sock);
-   tls_stream = mongoc_stream_tls_new_with_hostname (
+  stream = mongoc_stream_socket_new(conn_sock);
+  tls_stream = mongoc_stream_tls_new_with_hostname(
       stream, "kms.us-east-1.amazonaws.com", &ssl_opts, 1 /* client */);
 
-   if (!tls_stream) {
-      SET_CRYPT_ERR ("could not create TLS stream on AWS");
-      mongoc_stream_destroy (stream);
-      return NULL;
-   }
+  if (!tls_stream) {
+    SET_CRYPT_ERR("could not create TLS stream on AWS");
+    mongoc_stream_destroy(stream);
+    return NULL;
+  }
 
-   if (!mongoc_stream_tls_handshake_block (
+  if (!mongoc_stream_tls_handshake_block(
           tls_stream, "kms.us-east-1.amazonaws.com", 1000, &bson_error)) {
-      mongoc_stream_destroy (tls_stream);
-      _bson_to_mongocrypt_error (&bson_error, error);
-      return NULL;
-   }
+    mongoc_stream_destroy(tls_stream);
+    _bson_to_mongocrypt_error(&bson_error, error);
+    return NULL;
+  }
 
-   return tls_stream;
+  return tls_stream;
 }
 
-static void
-print_without_carriage_return (uint8_t *buf, ssize_t n)
-{
-   ssize_t i;
+static void print_without_carriage_return(uint8_t *buf, ssize_t n) {
+  ssize_t i;
 
-   for (i = 0; i < n; i++) {
-      if (buf[i] != '\r') {
-         putchar (buf[i]);
-      }
-   }
+  for (i = 0; i < n; i++) {
+    if (buf[i] != '\r') {
+      putchar(buf[i]);
+    }
+  }
 }
 
-static bool
-_api_call (mongoc_crypt_t *crypt,
-   kms_request_t *request,
-           kms_response_t **response,
-           mongoc_crypt_error_t *error)
-{
-   bool ret = false;
-   mongoc_stream_t *stream;
-   char *sreq = NULL;
-   size_t sreq_len;
-   ssize_t n;
-   uint8_t read_buf[64];
-   kms_response_parser_t *parser = kms_response_parser_new ();
-   int64_t start;
-   const int32_t timeout_msec = 1000;
+static bool _api_call(mongoc_crypt_t *crypt, kms_request_t *request,
+                      kms_response_t **response, mongoc_crypt_error_t *error) {
+  bool ret = false;
+  mongoc_stream_t *stream;
+  char *sreq = NULL;
+  size_t sreq_len;
+  ssize_t n;
+  uint8_t read_buf[64];
+  kms_response_parser_t *parser = kms_response_parser_new();
+  int64_t start;
+  const int32_t timeout_msec = 1000;
 
-   stream = _get_aws_stream (error);
-   if (!stream) {
+  stream = _get_aws_stream(error);
+  if (!stream) {
+    goto cleanup;
+  }
+
+  /* TODO: CRLF endings? */
+  sreq = kms_request_get_signed(request);
+  sreq_len = strlen(sreq);
+
+  n = mongoc_stream_write(stream, sreq, sreq_len, timeout_msec);
+  /* TODO: don't error, just keep writing. */
+  if (n != (ssize_t)sreq_len) {
+    SET_CRYPT_ERR("Only wrote %zd of %zu bytes (errno: %d)\n", n, sreq_len,
+                  errno);
+    goto cleanup;
+  }
+
+  start = bson_get_monotonic_time();
+  while (kms_response_parser_wants_bytes(parser, sizeof(read_buf))) {
+    if (bson_get_monotonic_time() - start > timeout_msec * 1000) {
+      SET_CRYPT_ERR("Timed out reading response\n");
       goto cleanup;
-   }
+    }
 
-   /* TODO: CRLF endings? */
-   sreq = kms_request_get_signed (request);
-   sreq_len = strlen (sreq);
-   printf ("%s\n", sreq);
-
-   n = mongoc_stream_write (stream, sreq, sreq_len, timeout_msec);
-   /* TODO: don't error, just keep writing. */
-   if (n != (ssize_t) sreq_len) {
-      SET_CRYPT_ERR (
-               "Only wrote %zd of %zu bytes (errno: %d)\n",
-               n,
-               sreq_len,
-               errno);
+    n = mongoc_stream_read(stream, read_buf, sizeof(read_buf), 1, timeout_msec);
+    if (n < 0) {
+      SET_CRYPT_ERR("Read returned %zd (errno: %d)\n", n, errno);
       goto cleanup;
-   }
+    }
 
-   start = bson_get_monotonic_time ();
-   while (kms_response_parser_wants_bytes (parser, sizeof (read_buf))) {
-      if (bson_get_monotonic_time () - start > timeout_msec * 1000) {
-         SET_CRYPT_ERR ("Timed out reading response\n");
-         goto cleanup;
-      }
-
-      n = mongoc_stream_read (
-         stream, read_buf, sizeof (read_buf), 1, timeout_msec);
-      if (n < 0) {
-         SET_CRYPT_ERR ("Read returned %zd (errno: %d)\n", n, errno);
-         goto cleanup;
-      }
-
-      if (n == 0) {
-         break;
-      }
-
-      print_without_carriage_return (read_buf, n);
-      kms_response_parser_feed (parser, read_buf, (uint32_t) n);
-   }
-
-   *response = kms_response_parser_get_response (parser);
-   if (!*response) {
-      SET_CRYPT_ERR ("Could not get kms response");
-      goto cleanup;
-   }
-
-   ret = true;
-cleanup:
-   kms_response_parser_destroy (parser);
-   bson_free (sreq);
-   mongoc_stream_destroy (stream);
-   return ret;
-}
-
-static bool
-_get_data_key_from_response (kms_response_t* response, mongoc_crypt_key_t* key, mongoc_crypt_error_t* error) {
-   bson_json_reader_t* reader = NULL;
-   const char* raw_response_body;
-   bson_t response_body = BSON_INITIALIZER;
-   bson_iter_t iter;
-   reader = bson_json_data_reader_new (false, 1024);
-   bool ret = false;
-   char* b64_str;
-   uint32_t b64_strlen;
-   uint8_t* decoded_data;
-   int decoded_len;
-   bson_error_t bson_error;
-
-   raw_response_body = kms_response_get_body (response);
-   bson_json_data_reader_ingest (reader, (const uint8_t*) raw_response_body, strlen(raw_response_body));
-   switch (bson_json_reader_read(reader, &response_body, &bson_error)) {
-   case 1:
+    if (n == 0) {
       break;
-   case -1:
-      _bson_to_mongocrypt_error (&bson_error, error);
-      goto cleanup;
-   default:
-      SET_CRYPT_ERR ("Could not read JSON document from response");
-      goto cleanup;
-   }
+    }
 
-   CRYPT_TRACE ("kms response: %s", tmp_json(&response_body));
+    /* print_without_carriage_return(read_buf, n); */
+    kms_response_parser_feed(parser, read_buf, (uint32_t)n);
+  }
 
-   if (!bson_iter_init_find (&iter, &response_body, "Plaintext")) {
-      SET_CRYPT_ERR ("JSON response does not include Plaintext");
-      goto cleanup;
-   }
+  *response = kms_response_parser_get_response(parser);
+  if (!*response) {
+    SET_CRYPT_ERR("Could not get kms response");
+    goto cleanup;
+  }
 
-   b64_str = (char*)bson_iter_utf8 (&iter, &b64_strlen);
-   CRYPT_TRACE("response plaintext: %s, %d\n", b64_str, b64_strlen);
-   /* We need to doubly base64 decode. */
-   decoded_data = bson_malloc(b64_strlen + 1);
-   decoded_len = kms_message_b64_pton (b64_str, decoded_data, b64_strlen);
-
-   b64_str = (char*) decoded_data;
-   b64_str[decoded_len + 1] = '\0';
-   CRYPT_TRACE("decryption #1: %s\n", b64_str);
-   decoded_data = bson_malloc((size_t)decoded_len + 1);
-
-   decoded_len = kms_message_b64_pton(b64_str, decoded_data, (size_t)decoded_len);
-   decoded_data[decoded_len] = '\0';
-   CRYPT_TRACE ("decryption #2: %s\n", (char*)decoded_data);
-
-   bson_free (b64_str);
-
-   key->data_key.data = decoded_data;
-   key->data_key.len = (uint32_t)decoded_len;
-   key->data_key.owned = true;
-   ret = true;
+  ret = true;
 cleanup:
-   bson_destroy (&response_body);
-   bson_json_reader_destroy (reader);
-   return ret;
+  kms_response_parser_destroy(parser);
+  bson_free(sreq);
+  mongoc_stream_destroy(stream);
+  return ret;
 }
 
+static bool _get_data_key_from_response(kms_response_t *response,
+                                        mongoc_crypt_key_t *key,
+                                        mongoc_crypt_error_t *error) {
+  bson_json_reader_t *reader = NULL;
+  const char *raw_response_body;
+  bson_t response_body = BSON_INITIALIZER;
+  bson_iter_t iter;
+  reader = bson_json_data_reader_new(false, 1024);
+  bool ret = false;
+  char *b64_str;
+  uint32_t b64_strlen;
+  uint8_t *decoded_data;
+  int decoded_len;
+  bson_error_t bson_error;
 
-bool
-_mongoc_crypt_kms_decrypt (mongoc_crypt_t *crypt,
-                           mongoc_crypt_key_t *key,
-                           mongoc_crypt_error_t *error)
-{
-   kms_request_t* request = NULL;
-   kms_response_t* response = NULL;
-   kms_request_opt_t* request_opt = NULL;
-   bool ret = false;
+  raw_response_body = kms_response_get_body(response);
+  bson_json_data_reader_ingest(reader, (const uint8_t *)raw_response_body,
+                               strlen(raw_response_body));
+  switch (bson_json_reader_read(reader, &response_body, &bson_error)) {
+  case 1:
+    break;
+  case -1:
+    _bson_to_mongocrypt_error(&bson_error, error);
+    goto cleanup;
+  default:
+    SET_CRYPT_ERR("Could not read JSON document from response");
+    goto cleanup;
+  }
 
-   request_opt = kms_request_opt_new ();
-   kms_request_opt_set_connection_close (request_opt, true);
+  CRYPT_TRACE("kms response: %s", tmp_json(&response_body));
 
-   request = kms_decrypt_request_new (
-      key->key_material.data, key->key_material.len /* - 1 ? */, request_opt);
-   kms_request_set_region (request, crypt->opts->aws_region);
-   kms_request_set_service (request, "kms"); /* That seems odd. */
-   kms_request_set_access_key_id (request, crypt->opts->aws_access_key_id);
-   kms_request_set_secret_key (request, crypt->opts->aws_secret_access_key);
+  if (!bson_iter_init_find(&iter, &response_body, "Plaintext")) {
+    SET_CRYPT_ERR("JSON response does not include Plaintext");
+    goto cleanup;
+  }
 
-   if (!_api_call(crypt, request, &response, error)) {
-      goto cleanup;
-   }
+  b64_str = (char *)bson_iter_utf8(&iter, &b64_strlen);
+  CRYPT_TRACE("response plaintext: %s, %d\n", b64_str, b64_strlen);
+  /* We need to doubly base64 decode. */
+  decoded_data = bson_malloc(b64_strlen + 1);
+  decoded_len = kms_message_b64_pton(b64_str, decoded_data, b64_strlen);
 
-   if (!_get_data_key_from_response(response, key, error)) {
-      goto cleanup;
-   }
+  b64_str = (char *)decoded_data;
+  b64_str[decoded_len + 1] = '\0';
+  CRYPT_TRACE("decryption #1: %s\n", b64_str);
+  decoded_data = bson_malloc((size_t)decoded_len + 1);
 
-   ret = true;
+  decoded_len =
+      kms_message_b64_pton(b64_str, decoded_data, (size_t)decoded_len);
+  decoded_data[decoded_len] = '\0';
+  CRYPT_TRACE("decryption #2: %s\n", (char *)decoded_data);
+
+  bson_free(b64_str);
+
+  key->data_key.data = decoded_data;
+  key->data_key.len = (uint32_t)decoded_len;
+  key->data_key.owned = true;
+  ret = true;
 cleanup:
-   kms_request_opt_destroy (request_opt);
-   kms_request_destroy (request);
-   kms_response_destroy (response);
-   return ret;
+  bson_destroy(&response_body);
+  bson_json_reader_destroy(reader);
+  return ret;
+}
+
+bool _mongoc_crypt_kms_decrypt(mongoc_crypt_t *crypt, mongoc_crypt_key_t *key,
+                               mongoc_crypt_error_t *error) {
+  kms_request_t *request = NULL;
+  kms_response_t *response = NULL;
+  kms_request_opt_t *request_opt = NULL;
+  bool ret = false;
+
+  request_opt = kms_request_opt_new();
+  kms_request_opt_set_connection_close(request_opt, true);
+
+  request = kms_decrypt_request_new(
+      key->key_material.data, key->key_material.len /* - 1 ? */, request_opt);
+  kms_request_set_region(request, crypt->opts->aws_region);
+  kms_request_set_service(request, "kms"); /* That seems odd. */
+  kms_request_set_access_key_id(request, crypt->opts->aws_access_key_id);
+  kms_request_set_secret_key(request, crypt->opts->aws_secret_access_key);
+
+  if (!_api_call(crypt, request, &response, error)) {
+    goto cleanup;
+  }
+
+  if (!_get_data_key_from_response(response, key, error)) {
+    goto cleanup;
+  }
+
+  ret = true;
+cleanup:
+  kms_request_opt_destroy(request_opt);
+  kms_request_destroy(request);
+  kms_response_destroy(response);
+  return ret;
 }
--- src/mongocrypt-private.h
+++ src/mongocrypt-private.h
@@ -11,128 +11,112 @@
 #define MONGOC_CRYPT_TRACE
 
 #ifdef MONGOC_CRYPT_TRACE
-#define CRYPT_TRACE(...)                              \
-   do {                                               \
-      printf ("[CRYPT %s:%d] ", BSON_FUNC, __LINE__); \
-      printf (__VA_ARGS__);                           \
-      printf ("\n");                                  \
-   } while (0)
-#define CRYPT_ENTRY                                          \
-   do {                                                      \
-      printf ("[CRYPT entry] %s:%d\n", BSON_FUNC, __LINE__); \
-   } while (0)
+#define CRYPT_TRACE(...)                                                       \
+  do {                                                                         \
+    if (getenv("MONGOCRYPT_TRACE")) {                                          \
+      printf("[CRYPT %s:%d] ", BSON_FUNC, __LINE__);                           \
+      printf(__VA_ARGS__);                                                     \
+      printf("\n");                                                            \
+    }                                                                          \
+  } while (0)
+#define CRYPT_ENTRY                                                            \
+  do {                                                                         \
+    if (getenv("MONGOCRYPT_TRACE")) {                                          \
+      printf("[CRYPT entry] %s:%d\n", BSON_FUNC, __LINE__);                    \
+    }                                                                          \
+  } while (0)
 #else
 #define TRACE(msg, ...)
 #define ENTRY
 #endif
 
-const char *
-tmp_json (const bson_t *bson);
+const char *tmp_json(const bson_t *bson);
 
-void
-_mongoc_crypt_set_error (mongoc_crypt_error_t *error, /* OUT */
-                         uint32_t domain,     /* IN */
-                         uint32_t code,       /* IN */
-                         const char *format,  /* IN */
-                         ...);
+void _mongoc_crypt_set_error(mongoc_crypt_error_t *error, /* OUT */
+                             uint32_t domain,             /* IN */
+                             uint32_t code,               /* IN */
+                             const char *format,          /* IN */
+                             ...);
 
-void _bson_to_mongocrypt_error (const bson_error_t* bson_error, mongoc_crypt_error_t* error);
+void _bson_to_mongocrypt_error(const bson_error_t *bson_error,
+                               mongoc_crypt_error_t *error);
 
-   struct _mongoc_crypt_opts_t {
-   char *aws_region;
-   char *aws_secret_access_key;
-   char *aws_access_key_id;
-   char *mongocryptd_uri;
-   char *default_keyvault_client_uri;
+struct _mongoc_crypt_opts_t {
+  char *aws_region;
+  char *aws_secret_access_key;
+  char *aws_access_key_id;
+  char *mongocryptd_uri;
+  char *default_keyvault_client_uri;
 };
 
 struct _mongoc_crypt_t {
-   /* initially only one supported. Later, use from marking/ciphertext. */
-   mongoc_client_t *keyvault_client;
-   mongoc_client_t *mongocryptd_client;
-   mongoc_crypt_opts_t* opts;
+  /* initially only one supported. Later, use from marking/ciphertext. */
+  mongoc_client_t *keyvault_client;
+  mongoc_client_t *mongocryptd_client;
+  mongoc_crypt_opts_t *opts;
 };
-
 
 /* It's annoying passing around multiple values for bson binary values. */
 typedef struct {
-   uint8_t *data;
-   bson_subtype_t subtype;
-   uint32_t len;
-   bool owned;
+  uint8_t *data;
+  bson_subtype_t subtype;
+  uint32_t len;
+  bool owned;
 } mongoc_crypt_binary_t;
 
-void
-mongoc_crypt_binary_from_iter (bson_iter_t *iter, mongoc_crypt_binary_t *out);
-void
-mongoc_crypt_binary_from_iter_unowned (bson_iter_t *iter, mongoc_crypt_binary_t *out);
-void
-mongoc_crypt_binary_cleanup (mongoc_crypt_binary_t* binary);
-void
-mongoc_crypt_bson_append_binary (bson_t *bson,
-                                 const char *key,
-                                 uint32_t key_len,
-                                 mongoc_crypt_binary_t *in);
+void mongoc_crypt_binary_from_iter(bson_iter_t *iter,
+                                   mongoc_crypt_binary_t *out);
+void mongoc_crypt_binary_from_iter_unowned(bson_iter_t *iter,
+                                           mongoc_crypt_binary_t *out);
+void mongoc_crypt_binary_cleanup(mongoc_crypt_binary_t *binary);
+void mongoc_crypt_bson_append_binary(bson_t *bson, const char *key,
+                                     uint32_t key_len,
+                                     mongoc_crypt_binary_t *in);
 
 typedef struct {
-   bson_iter_t v_iter;
-   mongoc_crypt_binary_t iv;
-   /* one of the following is zeroed, and the other is set. */
-   mongoc_crypt_binary_t key_id;
-   const char *key_alt_name;
+  bson_iter_t v_iter;
+  mongoc_crypt_binary_t iv;
+  /* one of the following is zeroed, and the other is set. */
+  mongoc_crypt_binary_t key_id;
+  const char *key_alt_name;
 } mongoc_crypt_marking_t;
 
 /* consider renaming to encrypted_w_metadata? */
 typedef struct {
-   mongoc_crypt_binary_t e;
-   mongoc_crypt_binary_t iv;
-   mongoc_crypt_binary_t key_id;
+  mongoc_crypt_binary_t e;
+  mongoc_crypt_binary_t iv;
+  mongoc_crypt_binary_t key_id;
 } mongoc_crypt_encrypted_t;
 
 typedef struct {
-   mongoc_crypt_binary_t id;
-   mongoc_crypt_binary_t key_material;
-   mongoc_crypt_binary_t data_key;
+  mongoc_crypt_binary_t id;
+  mongoc_crypt_binary_t key_material;
+  mongoc_crypt_binary_t data_key;
 } mongoc_crypt_key_t;
 
-bool
-_mongoc_crypt_marking_parse_unowned (const bson_t *bson,
-                                     mongoc_crypt_marking_t *out,
-                                     mongoc_crypt_error_t* error);
-bool
-_mongoc_crypt_encrypted_parse_unowned (const bson_t *bson,
-                                       mongoc_crypt_encrypted_t *out,
-                                       mongoc_crypt_error_t* error);
-bool
-_mongoc_crypt_key_parse (const bson_t *bson,
-                         mongoc_crypt_key_t *out,
-                         mongoc_crypt_error_t* error);
+bool _mongoc_crypt_marking_parse_unowned(const bson_t *bson,
+                                         mongoc_crypt_marking_t *out,
+                                         mongoc_crypt_error_t *error);
+bool _mongoc_crypt_encrypted_parse_unowned(const bson_t *bson,
+                                           mongoc_crypt_encrypted_t *out,
+                                           mongoc_crypt_error_t *error);
+bool _mongoc_crypt_key_parse(const bson_t *bson, mongoc_crypt_key_t *out,
+                             mongoc_crypt_error_t *error);
 
-void mongoc_crypt_key_cleanup(mongoc_crypt_key_t* key);
+void mongoc_crypt_key_cleanup(mongoc_crypt_key_t *key);
 
-bool
-_mongoc_crypt_do_encryption (const uint8_t *iv,
-                             const uint8_t *key,
-                             const uint8_t *data,
-                             uint32_t data_len,
-                             uint8_t **out,
-                             uint32_t *out_len,
-                             mongoc_crypt_error_t* error);
+bool _mongoc_crypt_do_encryption(const uint8_t *iv, const uint8_t *key,
+                                 const uint8_t *data, uint32_t data_len,
+                                 uint8_t **out, uint32_t *out_len,
+                                 mongoc_crypt_error_t *error);
 
-
-bool
-_mongoc_crypt_do_decryption (const uint8_t *iv,
-                             const uint8_t *key,
-                             const uint8_t *data,
-                             uint32_t data_len,
-                             uint8_t **out,
-                             uint32_t *out_len,
-                             mongoc_crypt_error_t* error);
+bool _mongoc_crypt_do_decryption(const uint8_t *iv, const uint8_t *key,
+                                 const uint8_t *data, uint32_t data_len,
+                                 uint8_t **out, uint32_t *out_len,
+                                 mongoc_crypt_error_t *error);
 
 /* Modifies key */
-bool
-_mongoc_crypt_kms_decrypt (mongoc_crypt_t* crypt,
-                           mongoc_crypt_key_t* key,
-                           mongoc_crypt_error_t* error);
+bool _mongoc_crypt_kms_decrypt(mongoc_crypt_t *crypt, mongoc_crypt_key_t *key,
+                               mongoc_crypt_error_t *error);
 
 #endif
