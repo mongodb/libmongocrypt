ca346054a220612bcf893e27a066c73702f2b05b
clyde <clyde.bazile@10gen.com>
2019-05-09 19:47:48 +0000

    tcr

--- test/test-mongocrypt-ctx-encrypt.c
+++ test/test-mongocrypt-ctx-encrypt.c
@@ -674,6 +674,7 @@ _test_mongocrypt_buffer_from_iter (_mongocrypt_tester_t *tester)
    INIT;
 
    ROUNDTRIP ("int_key", "0B 00 00 00 10 00 63 C5 54 00 00", "63 C5 54 00");
+   plaintext.len += 1;
    _mongocrypt_buffer_to_bson_value (&plaintext, 0x10, &out);
 
    bson_destroy (bson);
