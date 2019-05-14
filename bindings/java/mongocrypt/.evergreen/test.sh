f51c441f9961cc366a3ca80fed72f0035778b2db
Jeff Yemin <jeff.yemin@10gen.com>
2019-05-14 14:21:32 -0400

    Add Evergreen testing of Java binding
    CDRIVER-2989

---
+++ bindings/java/mongocrypt/.evergreen/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Test the Java bindings for libmongocrypt
+
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+export JAVA_HOME="/opt/java/jdk8"
+
+if [ "Windows_NT" = "$OS" ]; then
+   export JAVA_HOME=/cygdrive/c/java/jdk8
+else
+   export JAVA_HOME=/opt/java/jdk8
+fi
+
+./gradlew -version
+
+./gradlew -Djna.library.path="../../../cmake-build" clean check
