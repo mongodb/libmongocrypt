e2bdbb99d209d6d39fd8e4890cd4be178afa4905
Kevin Albertson <kevin.albertson@mongodb.com>
2019-05-24 09:43:47 -0400

    CDRIVER-3080 warn if local schema has no encrypted fields

--- src/mongocrypt-ctx-encrypt.c
+++ src/mongocrypt-ctx-encrypt.c
@@ -173,6 +173,14 @@ _mongo_feed_markings (mongocrypt_ctx_t *ctx, mongocrypt_binary_t *in)
    if (bson_iter_init_find (&iter, &as_bson, "schemaRequiresEncryption") &&
        !bson_iter_as_bool (&iter)) {
       /* TODO: update cache: this schema does not require encryption. */
+      
+      /* If using a local schema, warn if there are no encrypted fields. */
+      if (!_mongocrypt_buffer_empty (&ctx->opts.local_schema)) {
+         _mongocrypt_log (
+            &ctx->crypt->log,
+            MONGOCRYPT_LOG_LEVEL_WARNING,
+            "local schema used but does not have encryption specifiers");
+      }
       return true;
    }
 
