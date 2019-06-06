92a2971113b0d3d8a85cf3b2b09b463a91659565
clyde <bazileclyde@gmail.com>
2019-06-06 15:08:00 -0400

    auto

--- src/mongocrypt-key-broker-private.h
+++ src/mongocrypt-key-broker-private.h
@@ -69,9 +69,8 @@ typedef struct {
    _mongocrypt_buffer_t filter;
    _mongocrypt_opts_t *crypt_opts;
    _mongocrypt_cache_t *cache_key;
-   _mongocrypt_key_broker_t;
    _mongocrypt_log_t *log;
-};
+} _mongocrypt_key_broker_t;
 
 
 void
