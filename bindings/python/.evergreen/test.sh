7163753c5f7f8c54ec45b4a6f171d74e8ebd5019
Shane Harvey <shnhrv@gmail.com>
2019-07-26 14:58:11 -0700

    Test all pythons on RHEL 6.2

--- bindings/python/.evergreen/test.sh
+++ bindings/python/.evergreen/test.sh
@@ -5,14 +5,19 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-# Supported/used environment variables:
-#   PYTHON_BINARY           The Python version to use.
-PYTHON_BINARY=${PYTHON_BINARY:-"/opt/python/3.6/bin/python3"}
-
 # This library is built by libmongocrypt/.evergreen/compile.sh
 evergreen_root="$(cd ../../../; pwd)"
 export MONGOCRYPT_LIB=${evergreen_root}/install/libmongocrypt/lib64/libmongocrypt.so
 
-$PYTHON_BINARY -c 'import sys; print(sys.version)'
+PYTHONS=("/opt/python/2.7/bin/python" \
+         "/opt/python/3.4/bin/python3" \
+         "/opt/python/3.5/bin/python3" \
+         "/opt/python/3.6/bin/python3" \
+         "/opt/python/pypy/bin/pypy" \
+         "/opt/python/pypy3.6/bin/pypy3")
 
-$PYTHON_BINARY setup.py test
+for PYTHON_BINARY in "${PYTHONS[@]}"; do
+    $PYTHON_BINARY -c 'import sys; print(sys.version)'
+
+    $PYTHON_BINARY setup.py test
+done
