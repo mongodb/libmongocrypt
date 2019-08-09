b2f1a225853e4b952ea71b868a758bf082449ee3
Shane Harvey <shnhrv@gmail.com>
2019-08-09 08:56:22 -0700

    PYTHON-1923 Test python bindings on Windows and macOS

--- bindings/python/.evergreen/test.sh
+++ bindings/python/.evergreen/test.sh
@@ -5,16 +5,28 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-# This library is built by libmongocrypt/.evergreen/compile.sh
+# libmongocrypt is built by libmongocrypt/.evergreen/compile.sh
 evergreen_root="$(cd ../../../; pwd)"
-export PYMONGOCRYPT_LIB=${evergreen_root}/install/libmongocrypt/lib64/libmongocrypt.so
 
-PYTHONS=("/opt/python/2.7/bin/python" \
-         "/opt/python/3.4/bin/python3" \
-         "/opt/python/3.5/bin/python3" \
-         "/opt/python/3.6/bin/python3" \
-#         "/opt/python/pypy/bin/pypy" \  # TODO: PyPy segfaults on RHEL 6.2
-         "/opt/python/pypy3.6/bin/pypy3")
+if [ "Windows_NT" = "$OS" ]; then # Magic variable in cygwin
+    PYMONGOCRYPT_LIB=${evergreen_root}/install/libmongocrypt/bin/mongocrypt.dll
+    export PYMONGOCRYPT_LIB=$(cygpath -m $PYMONGOCRYPT_LIB)
+
+    PYTHONS=("python")
+elif [ "Darwin" = "$(uname -s)" ]; then
+    export PYMONGOCRYPT_LIB=${evergreen_root}/install/libmongocrypt/lib/libmongocrypt.dylib
+
+    PYTHONS=("python")
+else
+    export PYMONGOCRYPT_LIB=${evergreen_root}/install/libmongocrypt/lib64/libmongocrypt.so
+
+    PYTHONS=("/opt/python/2.7/bin/python" \
+             "/opt/python/3.4/bin/python3" \
+             "/opt/python/3.5/bin/python3" \
+             "/opt/python/3.6/bin/python3" \
+    #         "/opt/python/pypy/bin/pypy" \  # TODO: PyPy segfaults on RHEL 6.2
+             "/opt/python/pypy3.6/bin/pypy3")
+fi
 
 for PYTHON_BINARY in "${PYTHONS[@]}"; do
     # Clear cached eggs for different python versions.
