d3ec6802b9ae6469d660873bebf514c1eea896d4
Shane Harvey <shnhrv@gmail.com>
2019-10-17 14:53:28 -0700

    pymongocrypt 0.1b2
    Pin to libmongocrypt 1.0.0-beta5 in release wheels.
    Add Windows to release.sh.

--- bindings/python/release.sh
+++ bindings/python/release.sh
@@ -1,48 +1,68 @@
 #!/bin/bash -ex
 
-# This script should be run on macOS to create the following distributions:
+# This script should be run on macOS and Cygwin on Windows.
+# On macOS it will create the following distributions:
 # pymongocrypt-<version>.tar.gz
 # pymongocrypt-<version>-py2.py3-none-manylinux2010_x86_64.whl
 # pymongocrypt-<version>-py2.py3-none-macosx_10_9_x86_64.whl
+#
+# On Windows it will create the following distribution:
+# pymongocrypt-<version>-py2.py3-none-win_amd64.whl
 
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-# Build the source dist first
-rm -rf build
-python3.7 setup.py sdist
-
 # The libmongocrypt git revision release to embed in our wheels.
-REVISION=latest
+REVISION=$(git rev-parse 1.0.0-beta5)
 
-# Build the manylinux2010 wheel
-rm -rf ./libmongocrypt
-curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt/rhel-62-64-bit/master/${REVISION}/libmongocrypt.tar.gz
-mkdir libmongocrypt
-tar xzf libmongocrypt.tar.gz -C ./libmongocrypt
-NOCRYPTO_SO=libmongocrypt/nocrypto/lib64/libmongocrypt.so
-chmod +x ${NOCRYPTO_SO}
-cp ${NOCRYPTO_SO} pymongocrypt/
-rm -rf ./libmongocrypt libmongocrypt.tar.gz
+if [ "Windows_NT" = "$OS" ]; then # Magic variable in cygwin
+    rm -rf venv37
+    virtualenv -p C:\\python\\Python37\\python.exe venv37 && . ./venv37/Scripts/activate
 
-docker run --rm -v `pwd`:/python quay.io/pypa/manylinux2010_x86_64 /python/build-manylinux-wheel.sh
+    # Build the Windows wheel
+    rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
+    curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt/windows-test/master/${REVISION}/libmongocrypt.tar.gz
+    mkdir libmongocrypt
+    tar xzf libmongocrypt.tar.gz -C ./libmongocrypt
+    NOCRYPTO_SO=libmongocrypt/nocrypto/bin/mongocrypt.dll
+    chmod +x ${NOCRYPTO_SO}
+    cp ${NOCRYPTO_SO} pymongocrypt/
+    rm -rf ./libmongocrypt libmongocrypt.tar.gz
 
-rm -rf pymongocrypt/libmongocrypt.so
+    python setup.py bdist_wheel
+    rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
+    ls dist
+elif [ "Darwin" = "$(uname -s)" ]; then
+    # Build the source dist first
+    rm -rf build pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
+    python3.7 setup.py sdist
 
+    # Build the manylinux2010 wheel
+    rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
+    curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt/rhel-62-64-bit/master/${REVISION}/libmongocrypt.tar.gz
+    mkdir libmongocrypt
+    tar xzf libmongocrypt.tar.gz -C ./libmongocrypt
+    NOCRYPTO_SO=libmongocrypt/nocrypto/lib64/libmongocrypt.so
+    chmod +x ${NOCRYPTO_SO}
+    cp ${NOCRYPTO_SO} pymongocrypt/
+    rm -rf ./libmongocrypt libmongocrypt.tar.gz
 
-# Build the mac wheel
-rm -rf ./libmongocrypt
-curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt/macos/master/${REVISION}/libmongocrypt.tar.gz
-mkdir libmongocrypt
-tar xzf libmongocrypt.tar.gz -C ./libmongocrypt
-NOCRYPTO_SO=libmongocrypt/nocrypto/lib/libmongocrypt.dylib
-chmod +x ${NOCRYPTO_SO}
-cp ${NOCRYPTO_SO} pymongocrypt/
-rm -rf ./libmongocrypt libmongocrypt.tar.gz
+    docker run --rm -v `pwd`:/python quay.io/pypa/manylinux2010_x86_64 /python/build-manylinux-wheel.sh
 
-rm -rf build
-python3.7 setup.py bdist_wheel
+    # Build the mac wheel
+    rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
+    curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt/macos/master/${REVISION}/libmongocrypt.tar.gz
+    mkdir libmongocrypt
+    tar xzf libmongocrypt.tar.gz -C ./libmongocrypt
+    NOCRYPTO_SO=libmongocrypt/nocrypto/lib/libmongocrypt.dylib
+    chmod +x ${NOCRYPTO_SO}
+    cp ${NOCRYPTO_SO} pymongocrypt/
+    rm -rf ./libmongocrypt libmongocrypt.tar.gz
 
-rm -rf pymongocrypt/libmongocrypt.dylib
-
-ls dist
+    python3.7 setup.py bdist_wheel
+    rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
+    ls dist
+else
+   echo "ERROR: Run this script on macOS or Windows"
+   exit 1
+fi
