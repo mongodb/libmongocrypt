508c555440f5de4fce7d1661974055343041f40c
Matt Broadstone <mbroadst@mongodb.com>
2020-06-23 10:38:00 -0400

    fix: correct typo preventing passing a custom mongocrypt spawn path
    NODE-2667

--- bindings/node/test/mongocryptdManager.test.js
+++ bindings/node/test/mongocryptdManager.test.js
@@ -6,13 +6,11 @@ const expect = require('chai').expect;
 describe('MongocryptdManager', function() {
   it('should default to having spawnArgs of --idleShutdownTimeoutSecs=60', function() {
     const mcdm = new MongocryptdManager();
-
     expect(mcdm.spawnArgs).to.deep.equal(['--idleShutdownTimeoutSecs', 60]);
   });
 
   it('should concat --idleShutdownTimeoutSecs=60 to provided args', function() {
     const mcdm = new MongocryptdManager({ mongocryptdSpawnArgs: ['foo', 12] });
-
     expect(mcdm.spawnArgs).to.deep.equal(['foo', 12, '--idleShutdownTimeoutSecs', 60]);
   });
 
@@ -30,5 +28,21 @@ describe('MongocryptdManager', function() {
     });
 
     expect(mcdm.spawnArgs).to.deep.equal(['--idleShutdownTimeoutSecs=12']);
+  });
+
+  it('should support construction with options', function() {
+    const mcdm = new MongocryptdManager({
+      mongocryptdURI: 'some-uri',
+      mongocryptdBypassSpawn: true,
+      mongocryptdSpawnPath: 'some-spawn-path',
+      mongocryptdSpawnArgs: ['--idleShutdownTimeoutSecs=12']
+    });
+
+    expect(mcdm).to.eql({
+      uri: 'some-uri',
+      bypassSpawn: true,
+      spawnPath: 'some-spawn-path',
+      spawnArgs: ['--idleShutdownTimeoutSecs=12']
+    });
   });
 });
