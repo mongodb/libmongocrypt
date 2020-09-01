48dec941a480910dac11e46b97119f7bc43554e9
Kevin Albertson <kevin.albertson@mongodb.com>
2020-08-31 21:55:34 -0400

    MONGOCRYPT-270 add Azure KMS message requests

--- kms-message/test/test_kms_azure_online.c
+++ kms-message/test/test_kms_azure_online.c
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+#include <kms_message/kms_azure_request.h>
 #include <kms_message/kms_b64.h>
 #include <kms_message/kms_request.h>
 #include <kms_message/kms_response.h>
@@ -44,6 +45,8 @@ typedef struct {
    char *key_vault_url;
    char *key_path;
    char *key_host;
+   char *key_name;
+   char *key_version;
 } test_env_t;
 
 static char *
@@ -67,6 +70,8 @@ test_env_init (test_env_t *test_env)
    test_env->client_id = test_getenv ("AZURE_CLIENT_ID");
    test_env->client_secret = test_getenv ("AZURE_CLIENT_SECRET");
    test_env->key_url = test_getenv ("AZURE_KEY_URL");
+   test_env->key_name = test_getenv ("AZURE_KEY_NAME");
+   test_env->key_version = test_getenv ("AZURE_KEY_VERSION");
 
    loc = strstr (test_env->key_url, azure_domain);
    TEST_ASSERT (loc);
@@ -194,8 +199,6 @@ azure_authenticate (void)
 {
    kms_request_t *req;
    kms_request_opt_t *opt;
-   char *path;
-   char *payload;
    char *req_str;
    const char *res_str;
    bson_t *res_bson;
@@ -210,21 +213,12 @@ azure_authenticate (void)
    kms_request_opt_set_connection_close (opt, true);
    kms_request_opt_set_provider (opt, KMS_REQUEST_PROVIDER_AZURE);
 
-   path = bson_strdup_printf ("/%s/oauth2/v2.0/token", test_env.tenant_id);
-   payload = bson_strdup_printf (
-      "client_id=%s&scope=%s&client_secret=%s&grant_type=client_credentials",
-      test_env.client_id,
-      SCOPE,
-      test_env.client_secret);
-
-   req = kms_request_new ("POST", path, opt);
-   TEST_ASSERT (kms_request_add_header_field (
-      req, "Content-Type", "application/x-www-form-urlencoded"));
-   TEST_ASSERT (
-      kms_request_add_header_field (req, "Host", "login.microsoftonline.com"));
-   TEST_ASSERT (
-      kms_request_add_header_field (req, "Accept", "application/json"));
-   TEST_ASSERT (kms_request_append_payload (req, payload, strlen (payload)));
+   req = kms_azure_request_oauth_new ("login.microsoftonline.com",
+                                      SCOPE,
+                                      test_env.tenant_id,
+                                      test_env.client_id,
+                                      test_env.client_secret,
+                                      opt);
    req_str = kms_request_to_string (req);
    TEST_TRACE ("--> HTTP request:\n%s\n", req_str);
 
@@ -245,8 +239,6 @@ azure_authenticate (void)
    kms_request_free_string (req_str);
    kms_response_destroy (res);
    kms_request_destroy (req);
-   bson_free (path);
-   bson_free (payload);
    bson_destroy (res_bson);
    test_env_cleanup (&test_env);
    kms_request_opt_destroy (opt);
@@ -264,39 +256,37 @@ test_azure_wrapkey (void)
    char *req_str;
    char *bearer_token;
    kms_response_t *res;
-   char *path_and_query;
-   char *bearer_token_value;
-   char *payload;
    const char *res_str;
-   char *encrypted;
+   uint8_t *encrypted_raw;
+   size_t encrypted_raw_len;
    char *decrypted;
    bson_t *res_bson;
    bson_iter_t iter;
+   uint8_t *key_data;
+   char *key_data_b64url;
+   int i;
 
-/* value is 96 bytes, generated with openssl rand -base64 96. Then converted
- * to the base64url encoding, which Azure uses (slightly different from base64).
- */
-#define KEY_DATA_BASE64URL                                      \
-   "IyyjC2eyMNcYOgZaIXl1H0qTYZhdVbyyn-0kiSK0n9O-"               \
-   "5OPNLi9xdDnCO3VBSsI9cUWMtVfTwvL7HY8S1VCCUQDnTyx8ZPVNTSRZk_" \
-   "liS7BDsQjPEfC4LZv8Un3bSHs"
+#define KEYLEN 96
+
+   key_data = bson_malloc0 (KEYLEN);
+   for (i = 0; i < KEYLEN; i++) {
+      key_data[i] = i;
+   }
+   key_data_b64url = kms_message_raw_to_b64url (key_data, KEYLEN);
 
    test_env_init (&test_env);
-   path_and_query =
-      bson_strdup_printf ("%s/wrapkey?api-version=7.0", test_env.key_path);
    bearer_token = azure_authenticate ();
-   bearer_token_value = bson_strdup_printf ("Bearer %s", bearer_token);
-   payload = bson_strdup_printf (
-      "{\"alg\": \"RSA-OAEP-256\", \"value\": \"%s\"}", KEY_DATA_BASE64URL);
 
    opt = kms_request_opt_new ();
    kms_request_opt_set_connection_close (opt, true);
    kms_request_opt_set_provider (opt, KMS_REQUEST_PROVIDER_AZURE);
-   req = kms_request_new ("POST", path_and_query, opt);
-   kms_request_add_header_field (req, "Authorization", bearer_token_value);
-   kms_request_add_header_field (req, "Host", test_env.key_host);
-   kms_request_add_header_field (req, "Content-Type", "application/json");
-   kms_request_append_payload (req, payload, strlen (payload));
+   req = kms_azure_request_wrapkey_new (test_env.key_host,
+                                        bearer_token,
+                                        test_env.key_name,
+                                        test_env.key_version,
+                                        key_data,
+                                        KEYLEN,
+                                        opt);
    req_str = kms_request_to_string (req);
    TEST_TRACE ("--> HTTP request:\n%s\n", req_str);
    res = send_kms_request (req, test_env.key_host);
@@ -307,25 +297,22 @@ test_azure_wrapkey (void)
       bson_new_from_json ((const uint8_t *) res_str, strlen (res_str), NULL);
    TEST_ASSERT (res_bson);
    TEST_ASSERT (bson_iter_init_find (&iter, res_bson, "value"));
-   encrypted = bson_strdup (bson_iter_utf8 (&iter, NULL));
+   encrypted_raw = kms_message_b64url_to_raw (bson_iter_utf8 (&iter, NULL), &encrypted_raw_len);
+   TEST_ASSERT (encrypted_raw);
 
    bson_destroy (res_bson);
-   bson_free (payload);
    bson_free (req_str);
    kms_request_destroy (req);
    kms_response_destroy (res);
-   bson_free (path_and_query);
 
    /* Send a request to unwrap the encrypted key. */
-   path_and_query =
-      bson_strdup_printf ("%s/unwrapkey?api-version=7.0", test_env.key_path);
-   payload = bson_strdup_printf (
-      "{\"alg\": \"RSA-OAEP-256\", \"value\": \"%s\"}", encrypted);
-   req = kms_request_new ("POST", path_and_query, opt);
-   kms_request_add_header_field (req, "Authorization", bearer_token_value);
-   kms_request_add_header_field (req, "Host", test_env.key_host);
-   kms_request_add_header_field (req, "Content-Type", "application/json");
-   kms_request_append_payload (req, payload, strlen (payload));
+   req = kms_azure_request_unwrapkey_new (test_env.key_host,
+                                          bearer_token,
+                                          test_env.key_name,
+                                          test_env.key_version,
+                                          encrypted_raw,
+                                          encrypted_raw_len,
+                                          opt);
    req_str = kms_request_to_string (req);
    TEST_TRACE ("--> HTTP request:\n%s\n", req_str);
    res = send_kms_request (req, test_env.key_host);
@@ -336,18 +323,17 @@ test_azure_wrapkey (void)
    TEST_ASSERT (res_bson);
    TEST_ASSERT (bson_iter_init_find (&iter, res_bson, "value"));
    decrypted = bson_strdup (bson_iter_utf8 (&iter, NULL));
-   TEST_ASSERT_STREQUAL (decrypted, KEY_DATA_BASE64URL);
+   TEST_ASSERT_STREQUAL (decrypted, key_data_b64url);
 
    bson_destroy (res_bson);
    kms_response_destroy (res);
    bson_free (req_str);
-   bson_free (payload);
-   bson_free (path_and_query);
    bson_free (bearer_token);
-   bson_free (bearer_token_value);
    test_env_cleanup (&test_env);
    kms_request_destroy (req);
-   bson_free (encrypted);
+   bson_free (encrypted_raw);
+   bson_free (key_data_b64url);
+   bson_free (key_data);
    bson_free (decrypted);
    kms_request_opt_destroy (opt);
 }
@@ -355,6 +341,7 @@ test_azure_wrapkey (void)
 int
 main (int argc, char **argv)
 {
+   kms_message_init ();
    RUN_TEST (test_azure_wrapkey);
    return 0;
 }
