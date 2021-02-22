94a8e51c51849ac619e69110be5a350f1330f531
Shane Harvey <shnhrv@gmail.com>
2021-02-22 11:51:56 -0800

    BUMP pymongocrypt 1.1.1.dev0

--- bindings/python/pymongocrypt/version.py
+++ bindings/python/pymongocrypt/version.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '1.1.0'
+__version__ = '1.1.1.dev0'
 
 _MIN_LIBMONGOCRYPT_VERSION = '1.2.0'
