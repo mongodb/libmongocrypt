157dfc8a104bcd8f493093922883485ba1868820
Kevin Albertson <kevin.albertson@mongodb.com>
2021-06-29 15:27:23 -0400

    MONGOCRYPT-321 fix double free on oauth cache expiration (#178)

--- test/test-mongocrypt.h
+++ test/test-mongocrypt.h
@@ -274,6 +274,9 @@ _mongocrypt_tester_install_endpoint (_mongocrypt_tester_t *tester);
 void
 _mongocrypt_tester_install_kek (_mongocrypt_tester_t *tester);
 
+void
+_mongocrypt_tester_install_cache_oauth (_mongocrypt_tester_t *tester);
+
 /* Conveniences for getting test data. */
 
 /* Get a temporary bson_t from a JSON string. Do not free it. */
