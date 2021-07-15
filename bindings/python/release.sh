e798577ec6975cf6870d7d3c3fe32e1dff782070
Shane Harvey <shnhrv@gmail.com>
2021-07-15 15:02:51 -0700

    PYTHON-2814 Bundle libmongocrypt 1.2.1 in release wheels

--- bindings/python/release.sh
+++ bindings/python/release.sh
@@ -13,7 +13,7 @@ set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
 # The libmongocrypt git revision release to embed in our wheels.
-REVISION=$(git rev-list -n 1 1.2.0)
+REVISION=$(git rev-list -n 1 1.2.1)
 
 if [ "Windows_NT" = "$OS" ]; then # Magic variable in cygwin
     rm -rf venv37
