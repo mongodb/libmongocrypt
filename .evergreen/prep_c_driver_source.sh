cdc08e3be80801e9c02b92ac74e5c812a15b5575
Kevin Albertson <kevin.albertson@mongodb.com>
2021-09-21 13:20:28 -0400

    add to VERSION_CURRENT

--- .evergreen/prep_c_driver_source.sh
+++ .evergreen/prep_c_driver_source.sh
@@ -8,3 +8,4 @@ MONGO_C_DRIVER_VERSION=1.17.0
 
 # Force checkout with lf endings since .sh must have lf, not crlf on Windows
 git clone git@github.com:mongodb/mongo-c-driver.git --config core.eol=lf --config core.autocrlf=false --depth=1 --branch $MONGO_C_DRIVER_VERSION
+echo $MONGO_C_DRIVER_VERSION > mongo-c-driver/VERSION_CURRENT
