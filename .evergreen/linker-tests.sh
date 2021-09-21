bd13cf04be0a729ff118309e812718707e9f83e3
Kevin Albertson <kevin.albertson@mongodb.com>
2021-09-21 12:18:09 -0400

    remove clone-mongo-c-driver
    remove out-dated hard coded versions
    use --branch and --depth=1 for C driver clone

--- .evergreen/linker-tests.sh
+++ .evergreen/linker-tests.sh
@@ -41,7 +41,7 @@ mkdir -p linker_tests/{install,libmongocrypt-cmake-build,app-cmake-build}
 cd linker_tests
 
 # Make libbson1 and libbson2
-$libmongocrypt_root/.evergreen/clone-mongo-c-driver.sh
+$libmongocrypt_root/.evergreen/prep_c_driver_source.sh
 cd mongo-c-driver
 
 # Use C driver helper script to find cmake binary, stored in $CMAKE.
@@ -58,7 +58,7 @@ mkdir cmake-build
 cd cmake-build
 INSTALL_PATH="$(system_path $linker_tests_root/install/bson1)"
 SRC_PATH="$(system_path ../)"
-$CMAKE -DBUILD_VERSION=1.18.0-pre -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" "$SRC_PATH"
+$CMAKE -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" "$SRC_PATH"
 $CMAKE --build . --target install --config RelWithDebInfo
 # Make libbson2
 cd ..
@@ -67,7 +67,7 @@ git apply --ignore-whitespace "$(system_path $linker_tests_deps_root/bson_patche
 cd cmake-build
 INSTALL_PATH="$(system_path $linker_tests_root/install/bson2)"
 SRC_PATH="$(system_path ../)"
-$CMAKE -DBUILD_VERSION=1.18.0-pre -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" "$SRC_PATH"
+$CMAKE -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" "$SRC_PATH"
 $CMAKE --build . --target install --config RelWithDebInfo
 
 # Build libmongocrypt, static linking against libbson2
--- .evergreen/prep_c_driver_source.sh
+++ .evergreen/prep_c_driver_source.sh
@@ -3,14 +3,8 @@
 set -o xtrace
 set -o errexit
 
-./libmongocrypt/.evergreen/clone-mongo-c-driver.sh
-cd mongo-c-driver
+# Clone mongo-c-driver and check out to a tagged version.
+MONGO_C_DRIVER_VERSION=1.17.0
 
-if [ -z "$MONGO_C_DRIVER_VERSION" ]; then
-    echo "No MONGO_C_DRIVER_VERSION specified, calculating release version"
-    python ./build/calc_release_version.py > VERSION_CURRENT
-else
-    echo $MONGO_C_DRIVER_VERSION > VERSION_CURRENT
-fi
-
-cd ..
+# Force checkout with lf endings since .sh must have lf, not crlf on Windows
+git clone git@github.com:mongodb/mongo-c-driver.git --config core.eol=lf --config core.autocrlf=false --depth=1 --branch $MONGO_C_DRIVER_VERSION
