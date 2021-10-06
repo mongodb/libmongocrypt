3aa6dd4ed9402d21f21c8c268016e2d19b263232
Kevin Albertson <kevin.albertson@mongodb.com>
2021-10-06 16:48:03 -0400

    fix test

--- test/test-mongocrypt.c
+++ test/test-mongocrypt.c
@@ -727,9 +727,9 @@ _test_setopt_kms_providers (_mongocrypt_tester_t *tester)
       /* KMIP test cases. */
       {"{'kmip': { 'endpoint': '127.0.0.1:5696' }}", NULL},
       {"{'kmip': { }}", "expected endpoint kmip.endpoint"},
-      // KMIPTODO: localhost should be a valid endpoint for KMIP.
-      // Unlike Azure, GCP, and AWS, applications run their own KMIP servers.
-      {"{'kmip': { 'endpoint': 'localhost' }}", "Invalid endpoint"},
+      /* localhost is a valid endpoint for KMIP.
+       * Unlike Azure, GCP, and AWS, applications run their own KMIP servers. */
+      {"{'kmip': { 'endpoint': 'localhost' }}", NULL},
       {"{'kmip': { 'endpoint': '127.0.0.1:5696', 'extra': 'invalid' }}", "Unexpected field: 'extra'"}
    };
 
