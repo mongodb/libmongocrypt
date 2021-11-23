a65dca6987685de98951233b8a24d6f7468d0079
Durran Jordan <durran@gmail.com>
2021-11-23 14:39:23 +0100

    feat(NODE-3777): add node bindings for kms provider

--- bindings/node/src/mongocrypt.h
+++ bindings/node/src/mongocrypt.h
@@ -97,6 +97,7 @@ class MongoCryptKMSRequest : public Napi::ObjectWrap<MongoCryptKMSRequest> {
     Napi::Value Status(const Napi::CallbackInfo& info);
     Napi::Value Message(const Napi::CallbackInfo& info);
     Napi::Value BytesNeeded(const Napi::CallbackInfo& info);
+    Napi::Value KMSProvider(const Napi::CallbackInfo& info);
     Napi::Value Endpoint(const Napi::CallbackInfo& info);
 
    private:
