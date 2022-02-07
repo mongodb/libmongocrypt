9376d1fa134ae5b7eeac33b231276a5ede1a4a89
Neal Beeken <neal.beeken@mongodb.com>
2022-02-07 15:32:49 -0500

    test: fix incorrect done usage in beforeEach hook (#242)

--- bindings/node/test/autoEncrypter.test.js
+++ bindings/node/test/autoEncrypter.test.js
@@ -390,8 +390,8 @@ describe('AutoEncrypter', function () {
     });
   });
 
-  describe('noAutoSpawn', function (done) {
-    beforeEach('start MongocryptdManager', async function () {
+  describe('noAutoSpawn', function () {
+    beforeEach('start MongocryptdManager', function (done) {
       if (requirements.SKIP_LIVE_TESTS) {
         this.currentTest.skipReason = `requirements.SKIP_LIVE_TESTS=${requirements.SKIP_LIVE_TESTS}`;
         this.skip();
