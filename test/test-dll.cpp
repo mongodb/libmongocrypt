bb1ec69e8f1955ef740044109f934cbc0ba130ee
vector-of-bool <vectorofbool@gmail.com>
2022-02-10 22:20:44 -0700

    Basic test cases for library loading

---
+++ test/test-dll.cpp
@@ -0,0 +1,22 @@
+/**
+ * This file is not a test case. This file is to generate a dynamic library for
+ * testing the dll loading code.
+ */
+
+#include <string>
+#include <iostream>
+
+static std::string global_str;
+
+extern "C"
+#if _MSC_VER
+   __declspec(dllexport)
+#else
+__attribute__ ((visibility ("default")))
+#endif
+      int say_hello ()
+{
+   global_str = "Hello, DLL!";
+   std::cout << global_str << "\n";
+   return 42;
+}
