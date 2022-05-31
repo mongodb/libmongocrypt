76b8733d47e044fbe3fb4ed915477c4b7dc4db5c
vector-of-bool <vectorofbool@gmail.com>
2022-05-31 18:56:13 +0000

    Update Python bindings for csfle rename

--- bindings/python/pymongocrypt/binding.py
+++ bindings/python/pymongocrypt/binding.py
@@ -440,12 +440,12 @@ mongocrypt_setopt_encrypted_field_config_map (mongocrypt_t *crypt,
  * then the system's library-search mechanism will never be consulted.
  *
  * @note If an absolute path to the library is specified using
- * @ref mongocrypt_setopt_set_csfle_lib_path_override, then paths appended here
- * will have no effect.
+ * @ref mongocrypt_setopt_set_crypt_shared_lib_path_override, then paths
+ * appended here will have no effect.
  */
 void
-mongocrypt_setopt_append_csfle_search_path (mongocrypt_t *crypt,
-                                            const char *path);
+mongocrypt_setopt_append_crypt_shared_lib_search_path (mongocrypt_t *crypt,
+                                                       const char *path);
 
 /**
  * @brief Set a single override path for loading the CSFLE dynamic library.
@@ -462,16 +462,16 @@ mongocrypt_setopt_append_csfle_search_path (mongocrypt_t *crypt,
  * occur during the call to @ref mongocrypt_init.
  *
  * @note If a CSFLE library path override is specified here, then no paths given
- * to @ref mongocrypt_setopt_append_csfle_search_path will be consulted when
- * opening the CSFLE library.
+ * to @ref mongocrypt_setopt_append_crypt_shared_lib_search_path will be
+ * consulted when opening the CSFLE library.
  *
  * @note If a path is provided via this API and @ref mongocrypt_init fails to
  * initialize a valid CSFLE library instance for the path specified, then
  * the initialization of mongocrypt_t will fail with an error.
  */
 void
-mongocrypt_setopt_set_csfle_lib_path_override (mongocrypt_t *crypt,
-                                               const char *path);
+mongocrypt_setopt_set_crypt_shared_lib_path_override (mongocrypt_t *crypt,
+                                                      const char *path);
 
 /**
  * @brief Opt-into handling the MONGOCRYPT_CTX_NEED_KMS_CREDENTIALS state.
