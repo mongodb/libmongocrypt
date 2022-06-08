feb61cb1874c3c93978c00af1f7796d612b00c4e
vector-of-bool <vectorofbool@gmail.com>
2022-06-08 16:17:00 -0600

    Filter which files are given to clang-tidy

--- etc/list-compile-files.py
+++ etc/list-compile-files.py
@@ -2,8 +2,10 @@ description = """Prints the list of files in a compilation database.
 Run with python ./list-compile-files.py <path/to/compile_commands.json>
 """
 
+from fnmatch import fnmatch
 import sys
 import json
+import os
 
 if len(sys.argv) != 2:
     print(description)
@@ -11,5 +13,7 @@ if len(sys.argv) != 2:
 
 compile_db = json.load(open(sys.argv[1] + "compile_commands.json", "r"))
 for entry in compile_db:
-    print(entry["file"])
+    fname = os.path.basename(entry)
+    if fnmatch(fname, 'mc-*') or fnmatch(fname, 'mongocrypt-*'):
+        print(entry["file"])
 print("")
