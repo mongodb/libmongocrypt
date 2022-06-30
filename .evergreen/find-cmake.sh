ba4e3e939fc4c5e8ce5ca3959842e0ff8e0bd2bb
vector-of-bool <vectorofbool@gmail.com>
2022-06-30 21:42:56 +0000

    Unify indentation in find-cmake

--- .evergreen/find-cmake.sh
+++ .evergreen/find-cmake.sh
@@ -12,7 +12,7 @@ find_cmake ()
   elif [ -f "/opt/cmake/bin/cmake" ]; then
     CMAKE="/opt/cmake/bin/cmake"
   elif [ -z "$IGNORE_SYSTEM_CMAKE" ] && command -v cmake 2>/dev/null; then
-     CMAKE=cmake
+    CMAKE=cmake
   elif uname -a | grep -iq 'GNU/Linux'; then
     version="3.19.4"  # First version that ships arm64 binaries
     root="$PWD/cmake-$version"
@@ -28,21 +28,21 @@ find_cmake ()
     tar xzf "$PWD/cmake.tgz" -C "$root" --strip-components=1
     CMAKE=$expect_exe
   elif [ -f "/cygdrive/c/cmake/bin/cmake" ]; then
-     CMAKE="/cygdrive/c/cmake/bin/cmake"
+    CMAKE="/cygdrive/c/cmake/bin/cmake"
   fi
   if [ -z "$CMAKE" -o -z "$( $CMAKE --version 2>/dev/null )" ]; then
-     # Some images have no cmake yet, or a broken cmake (see: BUILD-8570)
-     echo "-- MAKE CMAKE --"
-     CMAKE_INSTALL_DIR=$(readlink -f cmake-install)
-     curl --retry 5 https://cmake.org/files/v3.11/cmake-3.11.0.tar.gz -sS --max-time 120 --fail --output cmake.tar.gz
-     tar xzf cmake.tar.gz
-     cd cmake-3.11.0
-     ./bootstrap --prefix="${CMAKE_INSTALL_DIR}"
-     make -j8
-     make install
-     cd ..
-     CMAKE="${CMAKE_INSTALL_DIR}/bin/cmake"
-     echo "-- DONE MAKING CMAKE --"
+    # Some images have no cmake yet, or a broken cmake (see: BUILD-8570)
+    echo "-- MAKE CMAKE --"
+    CMAKE_INSTALL_DIR=$(readlink -f cmake-install)
+    curl --retry 5 https://cmake.org/files/v3.11/cmake-3.11.0.tar.gz -sS --max-time 120 --fail --output cmake.tar.gz
+    tar xzf cmake.tar.gz
+    cd cmake-3.11.0
+    ./bootstrap --prefix="${CMAKE_INSTALL_DIR}"
+    make -j8
+    make install
+    cd ..
+    CMAKE="${CMAKE_INSTALL_DIR}/bin/cmake"
+    echo "-- DONE MAKING CMAKE --"
   fi
 }
 
