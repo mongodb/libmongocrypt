e189d2604bffe08e5edefe7ae002a3c7cf8016e3
Steven Silvester <steven.silvester@ieee.org>
2022-07-15 14:31:41 -0500

    fix explicit host platform

--- bindings/python/release.sh
+++ bindings/python/release.sh
@@ -57,7 +57,7 @@ elif [ "Darwin" = "$(uname -s)" ]; then
 
     # Explicitly set the host platform, see
     # https://discuss.python.org/t/pep-425-platform-tag/5157/4.
-    _PYTHON_HOST_PLATFORM="macosx-10.14-universal2" $PYTHON setup.py bdist_wheel
+    _PYTHON_HOST_PLATFORM="macosx-10.15-universal2" $PYTHON setup.py bdist_wheel
     rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
     ls dist
 fi
