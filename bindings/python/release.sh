a1b2047d728200406b6c882ff3cb2119ba5b9be2
Steven Silvester <steven.silvester@ieee.org>
2022-08-23 18:17:17 -0500

    clean up wheel building

--- bindings/python/release.sh
+++ bindings/python/release.sh
@@ -40,35 +40,14 @@ if [ "Windows_NT" = "$OS" ]; then # Magic variable in cygwin
     rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
     ls dist
 elif [ "Darwin" = "$(uname -s)" ]; then
-    PYTHON="/Library/Frameworks/Python.framework/Versions/3.7/bin/python3"
-
-    # Ensure updated deps.
-    $PYTHON -m pip install --upgrade pip setuptools wheel
-
-    # Build the source dist first.
-    rm -rf build pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
-    $PYTHON setup.py sdist
-
-    # Build the x86_64 only mac wheel.
-    rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
-    curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt-release/macos_x86_64/${BRANCH}/${REVISION}/libmongocrypt.tar.gz
-    mkdir libmongocrypt
-    tar xzf libmongocrypt.tar.gz -C ./libmongocrypt
-    NOCRYPTO_SO=libmongocrypt/nocrypto/lib/libmongocrypt.dylib
-    chmod +x ${NOCRYPTO_SO}
-    cp ${NOCRYPTO_SO} pymongocrypt/
-    rm -rf ./libmongocrypt libmongocrypt.tar.gz
-
-    $PYTHON setup.py bdist_wheel
-
-    # Build the universal mac wheel.
-    PYTHON="/Library/Frameworks/Python.framework/Versions/3.10/bin/python3"
+    # Build the mac wheel.
+    PYTHON=${PYTHON:="/Library/Frameworks/Python.framework/Versions/3.10/bin/python3"}
 
     # Ensure updated deps.
     $PYTHON -m pip install --upgrade pip setuptools wheel
 
     rm -rf build libmongocrypt pymongocrypt/*.so pymongocrypt/*.dll pymongocrypt/*.dylib
-    curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt-release/macos/${BRANCH}/${REVISION}/libmongocrypt.tar.gz
+    curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt-release/${MACOS_TARGET}/${BRANCH}/${REVISION}/libmongocrypt.tar.gz
     mkdir libmongocrypt
     tar xzf libmongocrypt.tar.gz -C ./libmongocrypt
     NOCRYPTO_SO=libmongocrypt/nocrypto/lib/libmongocrypt.dylib
