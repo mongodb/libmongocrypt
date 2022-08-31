f7824cb85afbb901e0c6eb16f409529ec58bdbc5
vector-of-bool <vectorofbool@gmail.com>
2022-08-31 23:37:28 +0000

    No path manip in clang-tidy.sh

--- .evergreen/clang-tidy.sh
+++ .evergreen/clang-tidy.sh
@@ -3,20 +3,19 @@
 # static analyzer.
 #
 
-set -euxo pipefail
+. "$(dirname "${BASH_SOURCE[0]}")/init.sh"
+
+set -o xtrace
+set -o errexit
 
 echo "Begin compile process"
 
-evergreen_root="$(pwd)"
-
-. ${evergreen_root}/libmongocrypt/.evergreen/setup-env.sh
-
-cd $evergreen_root
+. "$EVG_DIR/setup-env.sh"
 
 CLANG_TIDY=/opt/mongodbtoolchain/v3/bin/clang-tidy
 
 $CLANG_TIDY --version
 
-cd libmongocrypt
-
-python ./etc/list-compile-files.py ./cmake-build/ | xargs $CLANG_TIDY -p ./cmake-build
+python "$LIBMONGOCRYPT_DIR/etc/list-compile-files.py" \
+    "$LIBMONGOCRYPT_DIR/cmake-build/" \
+| xargs $CLANG_TIDY -p "$LIBMONGOCRYPT_DIR/cmake-build/"
