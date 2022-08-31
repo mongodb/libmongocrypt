fda63df35a2d57a9fbf217a2746c8c956b920d39
vector-of-bool <vectorofbool@gmail.com>
2022-08-31 23:37:28 +0000

    Use absolute paths in linker-tests.sh
    This maintains the previous layout of the linker-tests generated
    structure, but no longer relies on the working directory. All paths
    are specified explicitly.

--- .evergreen/linker-tests.sh
+++ .evergreen/linker-tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -euxo pipefail
-
 # Directory layout
 # .evergreen
 # -linker_tests_deps
@@ -19,24 +17,20 @@ set -euxo pipefail
 # --libmongocrypt
 #
 
-if [ ! -e ./.evergreen ]; then
-    echo "Error: run from libmongocrypt root"
-    exit 1;
-fi
+. "$(dirname "${BASH_SOURCE[0]}")/init.sh"
+. "$EVG_DIR/setup-env.sh"
 
-libmongocrypt_root=$(pwd)
-linker_tests_root=${libmongocrypt_root}/linker_tests
-linker_tests_deps_root=${libmongocrypt_root}/.evergreen/linker_tests_deps
+linker_tests_root="$LIBMONGOCRYPT_DIR/linker_tests"
+linker_tests_deps_root="$EVG_DIR/linker_tests_deps"
 
-. ${libmongocrypt_root}/.evergreen/setup-env.sh
-
-rm -rf linker_tests
-mkdir -p linker_tests/{install,libmongocrypt-cmake-build,app-cmake-build}
-cd linker_tests
+rm -rf -- "$linker_tests_root"
+mkdir -p "$linker_tests_root"/{install,libmongocrypt-cmake-build,app-cmake-build}
 
 # Make libbson1 and libbson2
-$libmongocrypt_root/.evergreen/prep_c_driver_source.sh
-cd mongo-c-driver
+pushd "$linker_tests_root"
+  . "$EVG_DIR/prep_c_driver_source.sh"
+  MONGOC_DIR="$linker_tests_root/mongo-c-driver"
+popd
 
 : "${ADDITIONAL_CMAKE_FLAGS:=}"
 : "${MACOS_UNIVERSAL:=}"
@@ -49,7 +43,7 @@ if [ "$OS_NAME" = "windows" ]; then
     fi
 else
     # Amazon Linux 2 (arm64) has a very old system CMake we want to ignore
-    IGNORE_SYSTEM_CMAKE=1 . "$libmongocrypt_root/.evergreen/find-cmake.sh"
+    IGNORE_SYSTEM_CMAKE=1 . "$EVG_DIR/find-cmake.sh"
     # Check if on macOS with arm64. Use system cmake. See BUILD-14565.
     OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
     MARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
@@ -62,40 +56,61 @@ if [ "${MACOS_UNIVERSAL-}" = "ON" ]; then
     ADDITIONAL_CMAKE_FLAGS="$ADDITIONAL_CMAKE_FLAGS -DCMAKE_OSX_ARCHITECTURES='arm64;x86_64'"
 fi
 
-git apply --ignore-whitespace "$(native_path $linker_tests_deps_root/bson_patches/libbson1.patch)"
-mkdir cmake-build
-cd cmake-build
-INSTALL_PATH="$(native_path $linker_tests_root/install/bson1)"
-SRC_PATH="$(native_path ../)"
-$CMAKE -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" "$SRC_PATH"
-$CMAKE --build . --target install --config RelWithDebInfo
+pushd "$MONGOC_DIR"
+  git apply --ignore-whitespace "$linker_tests_deps_root/bson_patches/libbson1.patch"
+popd
+
+BUILD_PATH="$MONGOC_DIR/cmake-build"
+BSON1_INSTALL_PATH="$linker_tests_root/install/bson1"
+SRC_PATH="$MONGOC_DIR"
+$CMAKE \
+  -DENABLE_MONGOC=OFF \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  $ADDITIONAL_CMAKE_FLAGS \
+  -DCMAKE_INSTALL_PREFIX="$BSON1_INSTALL_PATH" \
+  "-H$SRC_PATH" \
+  "-B$BUILD_PATH"
+$CMAKE --build "$BUILD_PATH" --target install --config RelWithDebInfo
 # Make libbson2
-cd ..
-git reset --hard
-git apply --ignore-whitespace "$(native_path $linker_tests_deps_root/bson_patches/libbson2.patch)"
-LIBBSON2_SRC_DIR="$(native_path "$PWD")"
+
+pushd "$MONGOC_DIR"
+  git reset --hard
+  git apply --ignore-whitespace "$linker_tests_deps_root/bson_patches/libbson2.patch"
+popd
+LIBBSON2_SRC_DIR="$MONGOC_DIR"
 
 # Build libmongocrypt, static linking against libbson2
-cd $linker_tests_root/libmongocrypt-cmake-build
-INSTALL_PATH="$(native_path $linker_tests_root/install/libmongocrypt)"
-SRC_PATH="$(native_path $libmongocrypt_root)"
-$CMAKE -DCMAKE_BUILD_TYPE=RelWithDebInfo "-DMONGOCRYPT_MONGOC_DIR=$LIBBSON2_SRC_DIR" $ADDITIONAL_CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" "$SRC_PATH"
-$CMAKE --build . --target install --config RelWithDebInfo
+BUILD_DIR="$linker_tests_root/libmongocrypt-cmake-build"
+LMC_INSTALL_PATH="$linker_tests_root/install/libmongocrypt"
+SRC_PATH="$LIBMONGOCRYPT_DIR"
+$CMAKE \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  "-DMONGOCRYPT_MONGOC_DIR=$LIBBSON2_SRC_DIR" \
+  $ADDITIONAL_CMAKE_FLAGS \
+  -DCMAKE_INSTALL_PREFIX="$LMC_INSTALL_PATH" \
+  "-H$SRC_PATH" \
+  "-B$BUILD_DIR"
+$CMAKE --build "$BUILD_DIR" --target install --config RelWithDebInfo
 
 echo "Test case: Modelling libmongoc's use"
 # app links against libbson1.so
 # app links against libmongocrypt.so
-cd $linker_tests_root/app-cmake-build
-PREFIX_PATH="$(native_path $linker_tests_root/install/bson1);$(native_path $linker_tests_root/install/libmongocrypt)"
-SRC_PATH="$(native_path $linker_tests_deps_root/app)"
-$CMAKE -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_PREFIX_PATH="$PREFIX_PATH" "$SRC_PATH"
-$CMAKE --build . --target app --config RelWithDebInfo
+BUILD_DIR="$linker_tests_root/app-cmake-build"
+PREFIX_PATH="$LMC_INSTALL_PATH;$BSON1_INSTALL_PATH"
+SRC_PATH="$linker_tests_deps_root/app"
+$CMAKE \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  $ADDITIONAL_CMAKE_FLAGS \
+  -DCMAKE_PREFIX_PATH="$PREFIX_PATH" \
+  "-H$SRC_PATH" \
+  "-B$BUILD_DIR"
+$CMAKE --build "$BUILD_DIR" --target app --config RelWithDebInfo
 
 if [ "$OS_NAME" = "windows" ]; then
-    export PATH="$PATH:$linker_tests_root/install/bson1/bin:$linker_tests_root/install/libmongocrypt/bin"
-    APP_CMD="./RelWithDebInfo/app.exe"
+    export PATH="$PATH:$BSON1_INSTALL_PATH/bin:$LMC_INSTALL_PATH/bin"
+    APP_CMD="$BUILD_DIR/RelWithDebInfo/app.exe"
 else
-    APP_CMD="./app"
+    APP_CMD="$BUILD_DIR/app"
 fi
 
 check_output () {
