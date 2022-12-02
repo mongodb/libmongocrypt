5ced49668a6b0dc6d11e9e62008f3a0290f0268b
vector-of-bool <vectorofbool@gmail.com>
2022-12-02 20:31:21 +0000

    Compat macros

--- src/mlib/macros.h
+++ src/mlib/macros.h
@@ -14,4 +14,31 @@
 #define MLIB_INIT(T) (T)
 #endif
 
+#ifdef __cplusplus
+#define _mlibCLinkageBegin extern "C" {
+#define _mlibCLinkageEnd }
+#else
+#define _mlibCLinkageBegin
+#define _mlibCLinkageEnd
+#endif
+
+/// Mark the beginning of a C-language-linkage section
+#define MLIB_C_LINKAGE_BEGIN _mlibCLinkageBegin
+/// End a C-language-linkage section
+#define MLIB_C_LINKAGE_END _mlibCLinkageEnd
+
+#if defined(__cpp_constexpr) && __cpp_constexpr >= 201304L
+#define _mlibConstexprFn constexpr inline
+#else
+#define _mlibConstexprFn inline
+#endif
+
+/**
+ * @brief Mark a function as constexpr
+ *
+ * Expands to `constexpr` in C++14 and above (and someday C26...?). "inline"
+ * otherwise.
+ */
+#define mlib_constexpr_fn _mlibConstexprFn
+
 #endif // MLIB_MACROS_H_INCLUDED
