95caf7693d05e1f4b5ca0aa01818f2460ec365b0
Roberto C. Sánchez <roberto@connexer.com>
2022-12-08 14:27:49 -0500

    fix typo
    Follow up for: 1a6cdc5dc824e9b1c484471b9010fc4f8329e7e5

--- kms-message/src/kms_kmip_reader_writer_private.h
+++ kms-message/src/kms_kmip_reader_writer_private.h
@@ -125,7 +125,7 @@ bool
 kmip_reader_read_string (kmip_reader_t *reader, uint8_t **ptr, size_t length);
 
 /* kmip_reader_find does not descend structures.
- * To find and descend into a structure use kmip_reader_find_and_get_struct_reader. */
+ * To find and descend into a structure use kmip_reader_find_and_recurse. */
 bool
 kmip_reader_find (kmip_reader_t *reader,
                   kmip_tag_type_t search_tag,
