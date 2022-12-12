50b64e38d22956c8fd89adce3d971b2444e343dd
vector-of-bool <vectorofbool@gmail.com>
2022-12-12 21:11:07 +0000

    PR tweaks and more tests
    - Check can compile as C
    - Better branch coverage by hitting each bit battern that has an effect
      on division.

--- src/mlib/int128.test.cpp
+++ src/mlib/int128.test.cpp
@@ -2,9 +2,10 @@
 
 #include <iostream>
 #include <random>
+#include <thread>
 #include <string>
 
-#if (defined(__GNUC__) && __GNUC__ < 7) || \
+#if (defined(__GNUC__) && __GNUC__ < 7 && !defined(__clang__)) || \
    (defined(_MSC_VER) && _MSC_VER < 1920)
 // Old GCC and old MSVC have partially-broken constexpr that prevents us from
 // properly using static_assert with from_string()
@@ -51,7 +52,16 @@ constexpr
    mlib_int128
    operator""_i128 (const char *s)
 {
-   return mlib_int128_from_string (s);
+   return mlib_int128_from_string (s, NULL);
+}
+
+#ifndef BROKEN_CONSTEXPR
+constexpr
+#endif
+   mlib_int128
+   operator""_i128 (const char *s, size_t)
+{
+   return mlib_int128_from_string (s, NULL);
 }
 
 #ifndef BROKEN_CONSTEXPR
@@ -135,7 +145,8 @@ template <typename Left> struct bound_lhs {
                info.expr);                                            \
       fprintf (stderr, "Expanded expression: ");                      \
       std::cerr << value << " " #Oper " " << rhs << '\n';             \
-      exit (2);                                                       \
+      std::exit (1);                                                  \
+      return {};                                                      \
    }
    DEFOP (==)
    DEFOP (!=)
@@ -206,6 +217,32 @@ static_assert (mlib_int128_negate (9223372036854775809_i128) <
                "fail");
 #endif
 
+static mlib_int128_divmod_result
+div_check (mlib_int128 num, mlib_int128 den)
+{
+   // std::cout << "Check: " << num << " ÷ " << den << '\n';
+   mlib_int128_divmod_result res = mlib_int128_divmod (num, den);
+#ifdef __SIZEOF_INT128__
+   // When we have an existing i128 impl, test against that:
+   __uint128_t num1;
+   __uint128_t den1;
+   memcpy (&num1, &num.r, sizeof num);
+   memcpy (&den1, &den.r, sizeof den);
+   __uint128_t q = num1 / den1;
+   __uint128_t r = num1 % den1;
+   mlib_int128_divmod_result expect;
+   memcpy (&expect.quotient.r, &q, sizeof q);
+   memcpy (&expect.remainder.r, &r, sizeof r);
+   CHECK (expect.quotient == res.quotient);
+   CHECK (expect.remainder == res.remainder);
+#endif
+   // Check inversion by multiplication provides the correct result
+   auto invert = mlib_int128_mul (res.quotient, den);
+   invert = mlib_int128_add (invert, res.remainder);
+   CHECK (invert == num);
+   return res;
+}
+
 // Runtime checks, easier to debug that static_asserts
 int
 main ()
@@ -240,35 +277,17 @@ main ()
    auto four_v2 = mlib_int128_lshift (eight, -1);
    CHECK (four == four_v2);
 
-   auto r = mlib_int128_divmod (27828649044156246570177174673037165454_i128,
-                                499242349997913298655486252455941907_i128);
+   // It's useful it specify bit patterns directly
+   auto in_binary =
+      0b110101010110100100001101111001111010100010111100100101101011010110101001010110110011000100000100011110010101101001111110001000_i128;
+   CHECK (in_binary == 70917759074396274376598533126648930184_i128);
+   CHECK (
+      in_binary ==
+      "0b110101010110100100001101111001111010100010111100100101101011010110101001010110110011000100000100011110010101101001111110001000"_i128);
 
-   CHECK (r.quotient == 55_i128);
-   CHECK (r.remainder == 370319794271015144125430787960360569_i128);
-
-   // Self-divide:
-   r = mlib_int128_divmod (628698094597401606590302208_i128,
-                           628698094597401606590302208_i128);
-   CHECK (r.quotient == 1_i128);
-   CHECK (r.remainder == 0_i128);
-
-   // With no high-32 bits in the denominator
-   r = mlib_int128_divmod (316356263640858117670580590964547584140_i128,
-                           13463362962560749016052695684_i128);
-   CHECK (r.quotient == 23497566285_i128);
-
-   // Remainder correctness with high bit set:
-   auto rem = mlib_int128_mod (292590981272581782572061492191999425232_i128,
-                               221673222198185508195462959065350495048_i128);
-   CHECK (rem == 70917759074396274376598533126648930184_i128);
-
-   // Remainder with 64bit denom:
-   rem = mlib_int128_mod (2795722437127403543495742528_i128, 708945413_i128);
-   CHECK (rem == 619266642_i128);
-
-   // 10-div:
-   CHECK (mlib_int128_div (MLIB_INT128_SMAX, 10_i128) ==
-          17014118346046923173168730371588410572_i128);
+   // Or hexadecimal
+   auto in_hex = 0x355a4379ea2f25ad6a56cc411e569f88_i128;
+   CHECK (in_hex == 70917759074396274376598533126648930184_i128);
 
    int8_t n = -12;
    CHECK (mlib_int128_scmp (zero, MLIB_INT128_CAST (n)) > 0);
@@ -278,28 +297,91 @@ main ()
    CHECK (std::string (mlib_int128_format (_2pow127).str) ==
           "170141183460469231731687303715884105728");
 
-   for (int i = 0; i < 100000; ++i) {
-      std::uniform_int_distribution<std::uint64_t> dist;
-      std::random_device r;
-      auto bits = r ();
-      mlib_int128 num = MLIB_INT128_FROM_PARTS (bits & 0b0001 * dist (r),
-                                                bits & 0b0010 * dist (r));
-      mlib_int128 denom = MLIB_INT128_FROM_PARTS (bits & 0b0100 * dist (r),
-                                                  bits & 0b1000 * dist (r));
-      if (mlib_int128_eq (denom, 0_i128)) {
-         continue;
+   auto r = div_check (27828649044156246570177174673037165454_i128,
+                       499242349997913298655486252455941907_i128);
+
+   CHECK (r.quotient == 55_i128);
+   CHECK (r.remainder == 370319794271015144125430787960360569_i128);
+
+   r = div_check (64208687961221311123721027584_i128, 3322092839076102144_i128);
+   CHECK (r.remainder == 3155565729965670400_i128);
+
+   // This division will trigger the rare Knuth 4.3.1D/D6 condition:
+   r = div_check (31322872034807296605612234499929458960_i128,
+                  34573864092216774938021667884_i128);
+   CHECK (r.quotient == 905969663_i128);
+   CHECK (r.remainder == 34573864092065898160364055868_i128);
+
+   // Self-divide:
+   r = div_check (628698094597401606590302208_i128,
+                  628698094597401606590302208_i128);
+   CHECK (r.quotient == 1_i128);
+   CHECK (r.remainder == 0_i128);
+
+   // With no high-32 bits in the denominator
+   r = div_check (316356263640858117670580590964547584140_i128,
+                  13463362962560749016052695684_i128);
+   CHECK (r.quotient == 23497566285_i128);
+
+   // Remainder correctness with high bit set:
+   r = div_check (292590981272581782572061492191999425232_i128,
+                  221673222198185508195462959065350495048_i128);
+   CHECK (r.remainder == 70917759074396274376598533126648930184_i128);
+
+   // Remainder with 64bit denom:
+   r = div_check (2795722437127403543495742528_i128, 708945413_i128);
+   CHECK (r.remainder == 619266642_i128);
+
+   // 10-div:
+   CHECK (mlib_int128_div (MLIB_INT128_SMAX, 10_i128) ==
+          17014118346046923173168730371588410572_i128);
+
+   std::random_device rd;
+   std::seed_seq seed ({rd (), rd (), rd (), rd ()});
+   // Pick every numerator bit pattern from 0b00'00 to 0b11'11
+   for (auto nbits = 0u; nbits < 16u; ++nbits) {
+      // This is an extremely rudimentary thread pool to parallelize the
+      // division checks. It doesn't need to be rigorous or optimal, it only
+      // needs to "just work."
+      std::vector<std::thread> threads;
+      threads.resize (15);
+      // Pick every denominator bit pattern from 0b00'01 to 0b11'11:
+      for (auto dbits = 1u; dbits < 16u; ++dbits) {
+         // Randomness:
+         std::mt19937 random;
+         random.seed (seed);
+         // Spawn a thread for this denominator bit pattern:
+         threads[dbits - 1] = std::thread{[nbits, dbits, random] () mutable {
+            std::uniform_int_distribution<std::uint32_t> dist;
+            // 100k random divisions:
+            for (auto i = 0; i < 100000; ++i) {
+               // Generate a denominator
+               auto den = 0_i128;
+               while (den == 0_i128) {
+                  // Regenerate until we don't have zero (very
+                  // unlikely, but be safe!)
+                  uint64_t dlo = 0, dhi = 0;
+                  (dbits & 1) && (dlo |= dist (random));
+                  (dbits & 2) && (dlo |= (uint64_t) dist (random) << 32);
+                  (dbits & 4) && (dhi |= dist (random));
+                  (dbits & 8) && (dhi |= (uint64_t) dist (random) << 32);
+                  den = MLIB_INT128_FROM_PARTS (dlo, dhi);
+               }
+               // Generate a numerator
+               uint64_t nlo = 0, nhi = 0;
+               (nbits & 1) && (nlo |= dist (random));
+               (nbits & 2) && (nlo |= (uint64_t) dist (random) << 32);
+               (nbits & 4) && (nhi |= dist (random));
+               (nbits & 8) && (nhi |= (uint64_t) dist (random) << 32);
+               mlib_int128 num = MLIB_INT128_FROM_PARTS (nlo, nhi);
+               // Divide them:
+               div_check (num, den);
+            }
+         }};
       }
-
-      // Attempt a division, even if we have no "reference" value
-      auto q = mlib_int128_divmod (num, denom);
-      (void) q;
-
-#ifdef __SIZEOF_INT128__
-      // When we have an existing i128 impl, test against that:
-      mlib_int128 exp = {.unsigned_ = (num.unsigned_ / denom.unsigned_)};
-      mlib_int128 exp2 = {.unsigned_ = (num.unsigned_ % denom.unsigned_)};
-      CHECK (q.quotient == exp);
-      CHECK (q.remainder == exp2);
-#endif
+      // Join the threads that are dividing:
+      for (auto &t : threads) {
+         t.join ();
+      }
    }
 }
