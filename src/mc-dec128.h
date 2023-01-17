8c7fa4905d1edc9589b0da9a02852ad394f66c2b
vector-of-bool <vectorofbool@gmail.com>
2023-01-17 20:58:18 +0000

    null-safe checks in string building

--- src/mc-dec128.h
+++ src/mc-dec128.h
@@ -520,7 +520,9 @@ mc_dec128_to_new_decimal_string (mc_dec128 d)
    if (mc_dec128_is_zero (d)) {
       // Just return "0"
       char *s = (char *) calloc (2, 1);
-      s[0] = '0';
+      if (s) {
+         s[0] = '0';
+      }
       return s;
    }
 
@@ -528,9 +530,14 @@ mc_dec128_to_new_decimal_string (mc_dec128 d)
       // Negate the result, return a string with a '-' prefix
       d = mc_dec128_negate (d);
       char *s = mc_dec128_to_new_decimal_string (d);
+      if (!s) {
+         return NULL;
+      }
       char *s1 = (char *) calloc (strlen (s) + 2, 1);
-      s1[0] = '-';
-      strcpy (s1 + 1, s);
+      if (s1) {
+         s1[0] = '-';
+         strcpy (s1 + 1, s);
+      }
       free (s);
       return s1;
    }
@@ -538,7 +545,9 @@ mc_dec128_to_new_decimal_string (mc_dec128 d)
    if (mc_dec128_is_inf (d) || mc_dec128_is_nan (d)) {
       const char *r = mc_dec128_is_inf (d) ? "Infinity" : "NaN";
       char *c = (char *) calloc (strlen (r) + 1, 1);
-      strcpy (c, r);
+      if (c) {
+         strcpy (c, r);
+      }
       return c;
    }
 
@@ -555,14 +564,16 @@ mc_dec128_to_new_decimal_string (mc_dec128 d)
       int64_t ndigits = mc_dec128_to_int64 (log10) + 1;
       // +1 for null
       char *strbuf = (char *) calloc ((size_t) (ndigits + 1), 1);
-      // Write the string backwards:
-      char *optr = strbuf + ndigits - 1;
-      while (!mc_dec128_is_zero (modf.whole)) {
-         mc_dec128 rem = mc_dec128_fmod (modf.whole, TEN);
-         int64_t remi = mc_dec128_to_int64 (rem);
-         *optr-- = DIGITS[remi];
-         // Divide ten
-         modf = mc_dec128_modf (mc_dec128_div (modf.whole, TEN));
+      if (strbuf) {
+         // Write the string backwards:
+         char *optr = strbuf + ndigits - 1;
+         while (!mc_dec128_is_zero (modf.whole)) {
+            mc_dec128 rem = mc_dec128_fmod (modf.whole, TEN);
+            int64_t remi = mc_dec128_to_int64 (rem);
+            *optr-- = DIGITS[remi];
+            // Divide ten
+            modf = mc_dec128_modf (mc_dec128_div (modf.whole, TEN));
+         }
       }
       return strbuf;
    } else if (mc_dec128_is_zero (modf.whole)) {
@@ -572,22 +583,36 @@ mc_dec128_to_new_decimal_string (mc_dec128 d)
       }
       // 'd' is now a whole number
       char *part = mc_dec128_to_new_decimal_string (d);
+      if (!part) {
+         return NULL;
+      }
       char *buf = (char *) calloc (strlen (part) + 3, 1);
-      buf[0] = '0';
-      buf[1] = '.';
-      strcpy (buf + 2, part);
+      if (buf) {
+         buf[0] = '0';
+         buf[1] = '.';
+         strcpy (buf + 2, part);
+      }
       free (part);
       return buf;
    } else {
       // We have both a whole part and a fractional part
       char *whole = mc_dec128_to_new_decimal_string (modf.whole);
+      if (!whole) {
+         return NULL;
+      }
       char *frac = mc_dec128_to_new_decimal_string (modf.frac);
+      if (!frac) {
+         free (whole);
+         return NULL;
+      }
       char *ret = (char *) calloc (strlen (whole) + strlen (frac) + 1, 1);
-      char *out = ret;
-      strcpy (out, whole);
-      out += strlen (whole);
-      // "frac" contains a leading zero, which we don't want
-      strcpy (out, frac + 1);
+      if (ret) {
+         char *out = ret;
+         strcpy (out, whole);
+         out += strlen (whole);
+         // "frac" contains a leading zero, which we don't want
+         strcpy (out, frac + 1);
+      }
       free (whole);
       free (frac);
       return ret;
