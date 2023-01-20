4a4cd1cc0d5583464123f380c100e9386111b35f
Kevin Albertson <kevin.albertson@mongodb.com>
2023-01-20 16:03:44 -0500

    MONGOCRYPT-519 wrap errors from processing crypt_shared markings (#538)
    * wrap errors from processing crypt_shared markings
    
    * note safety of wrapping errors

--- src/mongocrypt-private.h
+++ src/mongocrypt-private.h
@@ -66,7 +66,15 @@ tmp_json (const bson_t *bson);
 const char *
 tmp_buf (const _mongocrypt_buffer_t *buf);
 
-
+// _mongocrypt_set_error sets an error status.
+// `status->message` is set after evaluating `format` and variadic args.
+// It is safe to uses a `status->message` as an argument to wrap an error. For example:
+// _mongocrypt_set_error (
+//    status,
+//    MONGOCRYPT_STATUS_ERROR_CLIENT,
+//    MONGOCRYPT_GENERIC_ERROR_CODE,
+//    "Error occurred. Original error: %s",
+//    mongocrypt_status_message (status, NULL /* len */));
 MLIB_ANNOTATE_PRINTF (4, 5)
 void
 _mongocrypt_set_error (mongocrypt_status_t *status,
