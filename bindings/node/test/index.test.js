f49c6762603fb3d11ddd56e58a02a9c14c56da0c
Neal Beeken <neal.beeken@mongodb.com>
2023-02-09 13:04:05 -0500

    extension coverage

--- bindings/node/test/index.test.js
+++ bindings/node/test/index.test.js
@@ -18,4 +18,12 @@ describe('mongodb-client-encryption entrypoint', () => {
   it('should export all and only the expected keys in expected_exports', () => {
     expect(mongodbClientEncryption).to.have.all.keys(EXPECTED_EXPORTS);
   });
+
+  it('extension returns an object equal in shape to the default except for extension', () => {
+    const extensionResult = mongodbClientEncryption.extension(require('mongodb'));
+    const expectedExports = EXPECTED_EXPORTS.filter(exp => exp !== 'extension');
+    const exportsDefault = Object.keys(mongodbClientEncryption).filter(exp => exp !== 'extension');
+    expect(extensionResult).to.have.all.keys(expectedExports);
+    expect(extensionResult).to.have.all.keys(exportsDefault);
+  });
 });
