0b8c3d12429b3845292e4a853fa07cee59d31091
Kevin Albertson <kevin.albertson@mongodb.com>
2023-03-21 09:06:56 -0400

    format kms_kmip_request.c

--- kms-message/src/kms_kmip_request.c
+++ kms-message/src/kms_kmip_request.c
@@ -23,7 +23,8 @@
 #include <stdint.h>
 
 static void
-copy_writer_buffer (kms_request_t *req, kmip_writer_t *writer) {
+copy_writer_buffer (kms_request_t *req, kmip_writer_t *writer)
+{
    const uint8_t *buf;
    size_t buflen;
 
@@ -109,7 +110,8 @@ kms_kmip_request_register_secretdata_new (void *reserved,
    /* 0x01 = Raw */
    kmip_writer_write_enumeration (writer, KMIP_TAG_KeyFormatType, 0x01);
    kmip_writer_begin_struct (writer, KMIP_TAG_KeyValue);
-   kmip_writer_write_bytes (writer, KMIP_TAG_KeyMaterial, (const char *) data, len);
+   kmip_writer_write_bytes (
+      writer, KMIP_TAG_KeyMaterial, (const char *) data, len);
    kmip_writer_close_struct (writer); /* KMIP_TAG_KeyValue */
    kmip_writer_close_struct (writer); /* KMIP_TAG_KeyBlock */
    kmip_writer_close_struct (writer); /* KMIP_TAG_SecretData */
