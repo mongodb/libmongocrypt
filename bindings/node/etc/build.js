7e13ee3361e9210c6d365343f830834271339ba9
Martin Rodriguez Reboredo <yakoyoku@gmail.com>
2023-04-23 16:17:11 -0300

    fix(NODE-5143): vendor sources in node bindings

--- bindings/node/etc/build.js
+++ bindings/node/etc/build.js
@@ -1,12 +1,39 @@
 'use strict';
 
 const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
 const pkg = require(__dirname + '/../package.json');
+
+function copyRecursiveSync(src, dst) {
+  var exists = fs.existsSync(src);
+  var stats = exists && fs.statSync(src);
+  if (exists && stats.isDirectory()) {
+    fs.mkdirSync(dst);
+    fs.readdirSync(src).forEach(item => {
+      copyRecursiveSync(path.join(src, item), path.join(dst, item));
+    });
+  } else {
+    fs.copyFileSync(src, dst);
+  }
+}
+
+const vendorSourceDir = path.join(__dirname, '/../addon/mongocrypt');
+const links = ['CMakeLists.txt', 'cmake', 'etc', 'kms-message', 'src', 'third-party'];
 
 var argv = process.argv.slice(3); // strips ['node', 'build.js', subcommand]
 
 var command;
 switch (process.argv[2]) {
+  case 'preinstall':
+    if (!fs.existsSync(vendorSourceDir)) {
+      fs.mkdirSync(vendorSourceDir);
+      links.forEach(item => {
+        fs.symlinkSync(path.join('../../../../', item), path.join(vendorSourceDir, item));
+      });
+    }
+    process.exit(0);
+    break;
   case 'install':
     command = 'cmake-js';
     argv.unshift('compile');
@@ -14,6 +41,30 @@ switch (process.argv[2]) {
   case 'prebuild':
     command = 'prebuild';
     argv.push('--backend', 'cmake-js', '--');
+    break;
+  case 'prepack':
+    var exists = fs.existsSync(vendorSourceDir);
+    var stats = exists && fs.statSync(vendorSourceDir);
+    if (exists && stats.isDirectory()) {
+      fs.rmdirSync(vendorSourceDir, { recursive: true });
+    }
+    fs.mkdirSync(vendorSourceDir);
+    links.forEach(item => {
+      copyRecursiveSync(path.join('../../', item), path.join(vendorSourceDir, item));
+    });
+    fs.rmdirSync(path.join(vendorSourceDir, 'kms-message/aws-sig-v4-test-suite'), {
+      recursive: true
+    });
+    fs.rmdirSync(path.join(vendorSourceDir, 'kms-message/test'), { recursive: true });
+    process.exit(0);
+    break;
+  case 'postpack':
+    fs.rmdirSync(vendorSourceDir, { recursive: true });
+    fs.mkdirSync(vendorSourceDir);
+    links.forEach(item => {
+      fs.symlinkSync(path.join('../../../../', item), path.join(vendorSourceDir, item));
+    });
+    process.exit(0);
     break;
   default:
     throw new Error('unknown command ' + process.argv[2]);
--- bindings/node/test/release.test.ts
+++ bindings/node/test/release.test.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import * as tar from 'tar';
 import * as cp from 'child_process';
 import * as fs from 'fs';
+const path = require('path');
 
 const pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 const packFile = `mongodb-client-encryption-${pkg.version}.tgz`;
@@ -24,8 +25,34 @@ const REQUIRED_FILES = [
   'package/src/index.ts'
 ];
 
+function readdirRecursiveSync(dir) {
+  let res = [];
+  fs.readdirSync(dir, { withFileTypes: true }).forEach(item => {
+    const f = path.join(dir, item.name);
+    if (item.isDirectory()) {
+      res = [...res, ...readdirRecursiveSync(f).map(i => path.join(item.name, i))];
+    } else if (item.isSymbolicLink()) {
+      if (fs.statSync(f).isDirectory()) {
+        res = [...res, ...readdirRecursiveSync(f).map(i => path.join(item.name, i))];
+      } else {
+        res.push(item.name);
+      }
+    } else {
+      res.push(item.name);
+    }
+  });
+  return res;
+}
+
 describe(`Release ${packFile}`, function () {
   this.timeout(10000);
+
+  // This list is separate from `REQUIRED_FILES` due to its length, which is in the hundreds.
+  const vendoredSourceFileList = readdirRecursiveSync(path.join(__dirname, '../addon/mongocrypt'))
+    .filter(f => !(f.includes('/test/') || f.includes('/aws-sig-v4-test-suite/')))
+    .map(f => path.join('package/addon/mongocrypt', f))
+    .sort();
+  expect(vendoredSourceFileList).to.not.have.lengthOf(0, "Empty vendored sources");
 
   let tarFileList;
   before(() => {
@@ -51,8 +78,15 @@ describe(`Release ${packFile}`, function () {
     });
   }
 
+  for (const vendored of vendoredSourceFileList) {
+    it(`should contain ${vendored}`, () => {
+      expect(tarFileList).to.includes(vendored);
+    });
+  }
+
   it('should not have extraneous files', () => {
-    const unexpectedFileList = tarFileList.filter(f => !REQUIRED_FILES.some(r => r === f));
+    const requiredFileList = REQUIRED_FILES.concat(vendoredSourceFileList);
+    const unexpectedFileList = tarFileList.filter(f => !requiredFileList.some(r => r === f));
     expect(unexpectedFileList).to.have.lengthOf(0, `Extra files: ${unexpectedFileList.join(', ')}`);
   });
 });
