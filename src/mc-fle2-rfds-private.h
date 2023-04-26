47ef55d9c3f88130ee51d6aa206b3972c6e94b55
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2023-04-26 13:49:55 -0500

    Update src/mc-fle2-rfds-private.h
    Co-authored-by: Kevin Albertson <kevin.albertson@10gen.com>

--- src/mc-fle2-rfds-private.h
+++ src/mc-fle2-rfds-private.h
@@ -49,7 +49,7 @@ bool mc_FLE2RangeFindDriverSpec_parse(mc_FLE2RangeFindDriverSpec_t *spec,
 // mc_FLE2RangeFindDriverSpec_to_placeholders creates a new document with
 // placeholders to encrypt.
 //
-// ACHTUNG: `out` MUST be initialized!
+// `out` must be initialized by caller.
 bool mc_FLE2RangeFindDriverSpec_to_placeholders(mc_FLE2RangeFindDriverSpec_t *spec,
                                                 const mc_RangeOpts_t *range_opts,
                                                 int64_t maxContentionCounter,
