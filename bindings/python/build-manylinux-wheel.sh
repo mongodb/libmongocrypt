b350f8a6c0aeff51cb58f7951e3e53373c1f98e8
Shane Harvey <shnhrv@gmail.com>
2023-05-08 09:33:40 -0700

    PYTHON-3707 Stop using universal wheels

--- bindings/python/build-manylinux-wheel.sh
+++ bindings/python/build-manylinux-wheel.sh
@@ -4,7 +4,7 @@ cd /python
 # Compile wheel
 # https://github.com/pypa/manylinux/issues/49
 rm -rf build
-/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel --universal
+/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
 
 # Audit wheels and write manylinux tag
 for whl in dist/*.whl; do
