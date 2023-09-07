4ea143a20c086386617040572db8b22e6934ec23
Kevin Albertson <kevin.albertson@mongodb.com>
2023-09-07 13:52:59 -0400

    fix version specifier
    To fix error: 'Expected end or semicolon (after version specifier)' =

--- etc/format.sh
+++ etc/format.sh
@@ -10,7 +10,7 @@ if ! run_python -c ''; then
 fi
 
 # Check that we have a pipx of the proper version:
-run_python -c 'import pkg_resources; pkg_resources.require("pipx>=0.17.0<2.0")'
+run_python -c 'import pkg_resources; pkg_resources.require("pipx>=0.17.0,<2.0")'
 
 # Give default clang-format an empty string on stdin if there are no inputs files
 printf '' | run_python -m pipx run "clang-format==${CLANG_FORMAT_VERSION:?}" "$@"
