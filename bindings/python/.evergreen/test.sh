164f91355efbb0f9ceebe871a7c62c28a8a87369
Steven Silvester <steven.silvester@ieee.org>
2023-12-03 18:08:50 -0600

    fix handling of macos

--- bindings/python/.evergreen/test.sh
+++ bindings/python/.evergreen/test.sh
@@ -27,10 +27,7 @@ if [ "Windows_NT" = "$OS" ]; then # Magic variable in cygwin
 elif [ "Darwin" = "$(uname -s)" ]; then
     export PYMONGOCRYPT_LIB=${MONGOCRYPT_DIR}/nocrypto/lib/libmongocrypt.dylib
     local MACOS_VER=$(sw_vers --ProductVersion)
-    if [[ $(uname -m) == 'arm64' ]]; then
-      PYTHONS=("/Library/Frameworks/Python.framework/Versions/3.10/bin/python3"
-              )
-    elif [ $MACOS_VER == "10.14" ]; then
+    if [ $MACOS_VER == "10.14" ]; then
       PYTHONS=("python3"  # Python 3 from brew
                "/Library/Frameworks/Python.framework/Versions/3.7/bin/python3")
     else
