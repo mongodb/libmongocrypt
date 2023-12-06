03eedf80fad55f3c0de85820467f6bbc4169d78b
Steven Silvester <steven.silvester@ieee.org>
2023-12-06 14:04:57 -0600

    try again

---
+++ bindings/python/scripts/repair-linux-wheel.sh
@@ -0,0 +1,7 @@
+# Repair the linux wheel created by cibuildwheel
+wheel=$1
+dest_dir=$2
+rename s/(.*)-py3-none-.*.whl/$1-py3-none-any.whl/s $wheel
+root=$(dirname $wheel)
+ls $root"
+auditwheel repair -w $dest_dir $root/*.whl
