848e408ece9df42daa50cebf77619abcbd341e31
Steven Silvester <steven.silvester@ieee.org>
2023-12-06 16:30:19 -0600

    try this

--- bindings/python/scripts/repair-linux-wheel.sh
+++ bindings/python/scripts/repair-linux-wheel.sh
@@ -1,7 +1,6 @@
 # Repair the linux wheel created by cibuildwheel
 wheel=$1
 dest_dir=$2
-rename 's/(.*)-py3-none-.*.whl/$1-py3-none-any.whl/s' $wheel
-root=$(dirname $wheel)
-ls $root"
-auditwheel repair -w $dest_dir $root/*.whl
+new_name=$(echo "$wheel" | sed -E "s/(.*)-py3-none-.*.whl/\1-py3-none-any.whl"
+mv $wheel $new_name
+auditwheel repair -w $dest_dir $new_name
