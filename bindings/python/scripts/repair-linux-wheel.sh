37fd1fb1f8866289e214ef5c924732b287bf2a32
Steven Silvester <steven.silvester@ieee.org>
2023-12-06 16:35:14 -0600

    syntax

--- bindings/python/scripts/repair-linux-wheel.sh
+++ bindings/python/scripts/repair-linux-wheel.sh
@@ -1,6 +1,6 @@
 # Repair the linux wheel created by cibuildwheel
 wheel=$1
 dest_dir=$2
-new_name=$(echo "$wheel" | sed -E "s/(.*)-py3-none-.*.whl/\1-py3-none-any.whl"
+new_name=$(echo "$wheel" | sed -E "s/(.*)-py3-none-.*.whl/\1-py3-none-any.whl")
 mv $wheel $new_name
 auditwheel repair -w $dest_dir $new_name
