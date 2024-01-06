a5a42906090a4a57db73068a4348b7f52901b1cc
Steven Silvester <steven.silvester@ieee.org>
2024-01-05 19:26:58 -0600

    fix pythons

--- bindings/python/.evergreen/test.sh
+++ bindings/python/.evergreen/test.sh
@@ -45,19 +45,23 @@ elif [ "Darwin" = "$(uname -s)" ]; then
       --version latest --out ../crypt_shared/
 else
     export PYMONGOCRYPT_LIB=${MONGOCRYPT_DIR}/nocrypto/lib64/libmongocrypt.so
-    PYTHONS=("/opt/python/3.7/bin/python3"
-             "/opt/python/3.8/bin/python3"
-             "/opt/python/3.9/bin/python3"
-             "/opt/python/3.10/bin/python3"
-             "/opt/python/3.11/bin/python3"
-             "/opt/python/3.12/bin/python3"
-            )
+    
     export CRYPT_SHARED_PATH="../crypt_shared/lib/mongo_crypt_v1.so"
     MACHINE=$(uname -m)
     if [ $MACHINE == "aarch64" ]; then
         TARGET=rhel82
+        PYTHONS=("/opt/python/3.7/bin/python3"
+          "/opt/python/3.8/bin/python3"
+          "/opt/python/3.9/bin/python3"
+          "/opt/python/3.10/bin/python3"
+          "/opt/python/3.11/bin/python3"
+          "/opt/python/3.12/bin/python3"
+        )
     else
         TARGET=rhel80
+        PYTHONS=("/opt/mongodbtoolchain/v3/bin/python3"
+          "/opt/mongodbtoolchain/v4/bin/python3"
+        )
     fi
     /opt/mongodbtoolchain/v3/bin/python3 drivers-evergreen-tools/.evergreen/mongodl.py --component \
       crypt_shared --version latest --out ../crypt_shared/ --target $TARGET
