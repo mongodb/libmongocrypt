d16386ea5f12db08e7334253bcd5b120706d551b
Kevin Albertson <kevin.albertson@mongodb.com>
2024-02-02 09:42:53 -0500

    retain dots in keys when matching
    To interpret a pattern key "db.test" as a key, rather than a key path.

--- test/test-mongocrypt-assert-match-bson.c
+++ test/test-mongocrypt-assert-match-bson.c
@@ -889,9 +889,8 @@ const char *_mongoc_bson_type_to_str(bson_type_t t) {
 }
 
 void _assert_match_bson(const bson_t *doc, const bson_t *pattern) {
-    match_ctx_t ctx;
-
-    memset(&ctx, 0, sizeof(match_ctx_t));
+    // Set `retain_dots_in_keys` to interpret a pattern key "db.test" as a key, rather than a key path.
+    match_ctx_t ctx = {.retain_dots_in_keys = true};
     if (!match_bson_with_ctx(doc, pattern, &ctx)) {
         char *doc_str = doc ? bson_as_json(doc, NULL) : NULL;
         char *pattern_str = bson_as_json(pattern, NULL);
