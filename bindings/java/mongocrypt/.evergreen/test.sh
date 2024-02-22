5222397f261c3655461c15a6c5aea89336ac889e
Ross Lawley <ross@mongodb.com>
2024-02-22 15:23:26 +0000

    Java bindings updates (#754)
    * Gradle: Updated to latest (8.6)
    
    * Java - fallback to JDK8 if no JDK11
    
    MONGOCRYPT-590

--- bindings/java/mongocrypt/.evergreen/test.sh
+++ bindings/java/mongocrypt/.evergreen/test.sh
@@ -5,10 +5,19 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
+
 if [ "Windows_NT" = "$OS" ]; then
-   export JAVA_HOME=/cygdrive/c/java/jdk11
+   export JDK8="/cygdrive/c/java/jdk8"
+   export JDK11="/cygdrive/c/java/jdk11"
 else
-   export JAVA_HOME=/opt/java/jdk11
+  export JDK8="/opt/java/jdk8"
+  export JDK11="/opt/java/jdk11"
+fi
+
+if [ -d "$JDK11" ]; then
+  export JAVA_HOME=$JDK11
+else
+  export JAVA_HOME=$JDK8
 fi
 
 ./gradlew -version
