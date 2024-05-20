0eba3478a838ab03d86ae732ba6f21aff726afcf
Adrian Dole <adrian@dole.tech>
2024-05-20 10:43:16 -0700

    Support the Cray T3E Supercomputer where long long isn't an int64_t

--- src/mongocrypt.h
+++ src/mongocrypt.h
@@ -1158,7 +1158,6 @@ bool mongocrypt_kms_ctx_endpoint(mongocrypt_kms_ctx_t *kms, const char **endpoin
 MONGOCRYPT_EXPORT
 uint32_t mongocrypt_kms_ctx_bytes_needed(mongocrypt_kms_ctx_t *kms);
 
-
 /**
  * Indicates how long to sleep before sending this request.
  *
