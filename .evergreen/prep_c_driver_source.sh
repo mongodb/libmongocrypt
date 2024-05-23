1ba11dcccee4128fdafe81eb65e6642f77df82bc
Kevin Albertson <kevin.albertson@mongodb.com>
2024-05-22 20:30:05 -0400

    remove unnecessary VERSION_CURRENT
    VERSION_CURRENT is now committed into the C driver

--- .evergreen/prep_c_driver_source.sh
+++ .evergreen/prep_c_driver_source.sh
@@ -7,5 +7,3 @@ MONGO_C_DRIVER_VERSION=1.27.1
 
 # Force checkout with lf endings since .sh must have lf, not crlf on Windows
 git clone https://github.com/mongodb/mongo-c-driver.git --config core.eol=lf --config core.autocrlf=false --depth=1 --branch $MONGO_C_DRIVER_VERSION
-echo $MONGO_C_DRIVER_VERSION > mongo-c-driver/VERSION_CURRENT
-
