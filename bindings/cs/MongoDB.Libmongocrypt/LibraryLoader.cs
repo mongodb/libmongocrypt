befb72037cf84df4be6a82de9315c29750995dfa
adelinowona <adelinowona@gmail.com>
2024-06-27 09:22:42 -0400

    review comments

--- bindings/cs/MongoDB.Libmongocrypt/LibraryLoader.cs
+++ bindings/cs/MongoDB.Libmongocrypt/LibraryLoader.cs
@@ -179,6 +179,7 @@ namespace MongoDB.Libmongocrypt
                 }
                 catch
                 {
+                    _use_libdl1 = false;
                 }
             }
             
