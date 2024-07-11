ad81a3d2f401ecf01504741763ccf8e9f0f07893
Adrian Dole <adrian@dole.tech>
2024-07-11 11:29:53 -0700

    define default values

--- src/mc-range-encoding.c
+++ src/mc-range-encoding.c
@@ -616,3 +616,18 @@ bool mc_getTypeInfoDecimal128(mc_getTypeInfoDecimal128_args_t args,
 }
 
 #endif // defined MONGOCRYPT_HAVE_DECIMAL128_SUPPORT
+
+uint32_t trimFactorDefault(size_t maxlen, mc_optional_uint32_t trimFactor) {
+    if (trimFactor.set) {
+        return trimFactor.value;
+    }
+
+    if (mc_FLERangeTrimFactorDefault > maxlen - 1) {
+        return (uint32_t) maxlen - 1;
+    } else {
+        return mc_FLERangeTrimFactorDefault;
+    }
+}
+
+const uint32_t mc_FLERangeSparsityDefault = 2;
+const uint32_t mc_FLERangeTrimFactorDefault = 6;
