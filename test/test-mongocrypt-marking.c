636eac6f140509d440878a0c22c872efcd7eaac5
Kevin Albertson <kevin.albertson@mongodb.com>
2024-07-17 09:28:29 -0400

    remove out-dated test

--- test/test-mongocrypt-marking.c
+++ test/test-mongocrypt-marking.c
@@ -225,16 +225,6 @@ static void test_mc_get_mincover_from_FLE2RangeFindSpec(_mongocrypt_tester_t *te
                              "001\n"
                              "01\n"
                              "100000\n"},
-        {.description = "Range V2 enabled w/ no trim factor fails",
-         .findSpecJSON = RAW_STRING({
-             "lowerBound" : {"$numberInt" : "7"},
-             "lbIncluded" : true,
-             "upperBound" : {"$numberInt" : "32"},
-             "ubIncluded" : true,
-             "indexMin" : {"$numberInt" : "0"},
-             "indexMax" : {"$numberInt" : "32"}
-         }),
-         .expectedErrorAtParseTime = "Missing field 'trimFactor' in placeholder"},
         {.description = "Int32 Bounds included",
          .findSpecJSON = RAW_STRING({
              "lowerBound" : {"$numberInt" : "7"},
