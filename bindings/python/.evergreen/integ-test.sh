802aba05cf31955e96bf06bcf785650d94c6cbc4
Steven Silvester <steve.silvester@mongodb.com>
2024-09-04 15:53:58 -0500

    try again

--- bindings/python/.evergreen/integ-test.sh
+++ bindings/python/.evergreen/integ-test.sh
@@ -26,10 +26,8 @@ CRYPT_SHARED_DIR="$(pwd)/crypt_shared"
 
 if [ -e "${MONGOCRYPT_DIR}/lib64/" ]; then
     export PYMONGOCRYPT_LIB=${MONGOCRYPT_DIR}/nocrypto/lib64/libmongocrypt.so
-    PYMONGOCRYPT_LIB_CRYPTO=${MONGOCRYPT_DIR}/lib64/libmongocrypt.so
 else
     export PYMONGOCRYPT_LIB=${MONGOCRYPT_DIR}/nocrypto/lib/libmongocrypt.so
-    PYMONGOCRYPT_LIB_CRYPTO=${MONGOCRYPT_DIR}/lib/libmongocrypt.so
 fi
 
 createvirtualenv $PYTHON .venv
@@ -38,7 +36,7 @@ pushd $PYMONGO_DIR
 pip install -e ".[test,encryption]"
 source ${DRIVERS_TOOLS}/.evergreen/csfle/secrets-export.sh
 set -x
-TEST_ENCRYPTION=1 TEST_CRYPT_SHARED=1 DYLD_FALLBACK_LIBRARY_PATH=$CRYPT_SHARED_DIR/lib/:${DYLD_FALLBACK_LIBRARY_PATH:-} \
+TEST_CRYPT_SHARED=1 DYLD_FALLBACK_LIBRARY_PATH=$CRYPT_SHARED_DIR/lib/:${DYLD_FALLBACK_LIBRARY_PATH:-} \
     LD_LIBRARY_PATH=$CRYPT_SHARED_DIR/lib:${LD_LIBRARY_PATH-} \
     PATH=$CRYPT_SHARED_DIR/bin:$PATH \
     AUTH=auth SSL=ssl \
