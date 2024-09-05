0c2932055ccc84a008d375d582fec022ca112243
Steven Silvester <steve.silvester@mongodb.com>
2024-09-04 20:42:43 -0500

    debug

--- bindings/python/.evergreen/integ-test.sh
+++ bindings/python/.evergreen/integ-test.sh
@@ -12,14 +12,15 @@ BASE_PYTHON=$(find_python3)
 MONGOCRYPT_DIR="$MONGOCRYPT_DIR"
 
 if [ -e "${MONGOCRYPT_DIR}/lib64/" ]; then
-    export PYMONGOCRYPT_LIB=${MONGOCRYPT_DIR}/nocrypto/lib64/libmongocrypt.so
+    export PYMONGOCRYPT_LIB=${MONGOCRYPT_DIR}/lib64/libmongocrypt.so
 else
-    export PYMONGOCRYPT_LIB=${MONGOCRYPT_DIR}/nocrypto/lib/libmongocrypt.so
+    export PYMONGOCRYPT_LIB=${MONGOCRYPT_DIR}/lib/libmongocrypt.so
 fi
 
 createvirtualenv $BASE_PYTHON .venv
 pip install -e .
 pushd $PYMONGO_DIR
+python -c 'from pymongocrypt.binding import lib;assert lib.mongocrypt_is_crypto_available(), "mongocrypt_is_crypto_available() returned False"'
 pip install -e ".[test,encryption]"
 source ${DRIVERS_TOOLS}/.evergreen/csfle/secrets-export.sh
 set -x
