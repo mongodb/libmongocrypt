1addd285128f7e78ea7c47e5a2b75aa125847eed
Kevin Albertson <kevin.albertson@mongodb.com>
2025-08-06 20:34:13 -0400

    MONGOCRYPT-830 only include queried type in `FLE2TextSearchInsertSpec` (#1047)
    * skip ninja on macOS
    
    * only include queried type in `FLE2TextSearchInsertSpec`

--- test/test-mc-textopts.c
+++ test/test-mc-textopts.c
@@ -227,7 +227,93 @@ static void test_mc_TextOpts_to_FLE2TextSearchInsertSpec(_mongocrypt_tester_t *t
     }
 }
 
+static void test_mc_TextOpts_to_FLE2TextSearchInsertSpec_for_query(_mongocrypt_tester_t *tester) {
+    typedef struct {
+        const char *desc;
+        const char *in;
+        const char *v;
+        mongocrypt_query_type_t qt;
+        const char *expectError;
+        const char *expect;
+    } testcase;
+
+    testcase tests[] = {
+        {.desc = "Works with substring",
+         .in = RAW_STRING({
+             "caseSensitive" : true,
+             "diacriticSensitive" : false,
+             "substring" : {"strMaxLength" : 10, "strMinQueryLength" : 3, "strMaxQueryLength" : 8}
+         }),
+         .v = RAW_STRING({"v" : "test"}),
+         .qt = MONGOCRYPT_QUERY_TYPE_SUBSTRINGPREVIEW,
+         .expect = RAW_STRING(
+             {"v" : {"v" : "test", "casef" : true, "diacf" : false, "substr" : {"mlen" : 10, "ub" : 8, "lb" : 3}}})},
+        {.desc = "Works with prefix",
+         .in = RAW_STRING({
+             "caseSensitive" : true,
+             "diacriticSensitive" : false,
+             "prefix" : {"strMinQueryLength" : 3, "strMaxQueryLength" : 8}
+         }),
+         .v = RAW_STRING({"v" : "test"}),
+         .qt = MONGOCRYPT_QUERY_TYPE_PREFIXPREVIEW,
+         .expect =
+             RAW_STRING({"v" : {"v" : "test", "casef" : true, "diacf" : false, "prefix" : {"ub" : 8, "lb" : 3}}})},
+        {.desc = "Works with suffix",
+         .in = RAW_STRING({
+             "caseSensitive" : true,
+             "diacriticSensitive" : false,
+             "suffix" : {"strMinQueryLength" : 3, "strMaxQueryLength" : 8}
+         }),
+         .v = RAW_STRING({"v" : "test"}),
+         .qt = MONGOCRYPT_QUERY_TYPE_SUFFIXPREVIEW,
+         .expect =
+             RAW_STRING({"v" : {"v" : "test", "casef" : true, "diacf" : false, "suffix" : {"ub" : 8, "lb" : 3}}})},
+        {.desc = "Works with prefix + suffix when querying prefix",
+         .in = RAW_STRING({
+             "caseSensitive" : true,
+             "diacriticSensitive" : false,
+             "prefix" : {"strMinQueryLength" : 4, "strMaxQueryLength" : 9},
+             "suffix" : {"strMinQueryLength" : 3, "strMaxQueryLength" : 8}
+         }),
+         .v = RAW_STRING({"v" : "test"}),
+         .qt = MONGOCRYPT_QUERY_TYPE_PREFIXPREVIEW,
+         .expect =
+             RAW_STRING({"v" : {"v" : "test", "casef" : true, "diacf" : false, "prefix" : {"ub" : 9, "lb" : 4}}})},
+        {.desc = "Works with prefix + suffix when querying suffix",
+         .in = RAW_STRING({
+             "caseSensitive" : true,
+             "diacriticSensitive" : false,
+             "prefix" : {"strMinQueryLength" : 4, "strMaxQueryLength" : 9},
+             "suffix" : {"strMinQueryLength" : 3, "strMaxQueryLength" : 8}
+         }),
+         .v = RAW_STRING({"v" : "test"}),
+         .qt = MONGOCRYPT_QUERY_TYPE_SUFFIXPREVIEW,
+         .expect =
+             RAW_STRING({"v" : {"v" : "test", "casef" : true, "diacf" : false, "suffix" : {"ub" : 8, "lb" : 3}}})},
+    };
+
+    for (size_t i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+        testcase *test = tests + i;
+        mongocrypt_status_t *status = mongocrypt_status_new();
+        mc_TextOpts_t txo;
+        TEST_PRINTF("running test_mc_TextOpts_to_FLE2TextSearchInsertSpec subtest: %s\n", test->desc);
+        ASSERT_OK_STATUS(mc_TextOpts_parse(&txo, TMP_BSON_STR(test->in), status), status);
+        bson_t out = BSON_INITIALIZER;
+        bool ret =
+            mc_TextOpts_to_FLE2TextSearchInsertSpec_for_query(&txo, TMP_BSON_STR(test->v), test->qt, &out, status);
+        if (!test->expectError) {
+            ASSERT_OK_STATUS(ret, status);
+            ASSERT_EQUAL_BSON(TMP_BSON_STR(test->expect), &out);
+        } else {
+            ASSERT_FAILS_STATUS(ret, status, test->expectError);
+        }
+        bson_destroy(&out);
+        mongocrypt_status_destroy(status);
+    }
+}
+
 void _mongocrypt_tester_install_mc_TextOpts(_mongocrypt_tester_t *tester) {
     INSTALL_TEST(test_mc_TextOpts_parse);
     INSTALL_TEST(test_mc_TextOpts_to_FLE2TextSearchInsertSpec);
+    INSTALL_TEST(test_mc_TextOpts_to_FLE2TextSearchInsertSpec_for_query);
 }
