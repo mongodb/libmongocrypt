b3b3feff13ec88a0a5b94fbd28b1845ade3948e3
vector-of-bool <vectorofbool@gmail.com>
2026-01-14 13:18:35 -0700

    More robust env handling in build_all.sh

--- .evergreen/build_all.sh
+++ .evergreen/build_all.sh
@@ -7,63 +7,89 @@ echo "Begin compile process"
 
 . "$(dirname "${BASH_SOURCE[0]}")/setup-env.sh"
 
+set -eu
+
+# Poke our CMake runner to initialize it before we set environment variables
+# that could affect the CMake sub-build if we are building it from source.
+CFLAGS='' CXXFLAGS='' run_cmake --version
+
+# Directory where build files will be stored
+: "${BINARY_DIR:="$LIBMONGOCRYPT_DIR/cmake-build"}"
+
+# Control the build configuration that is generated.
+export CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-RelWithDebInfo}"
+# Sets the default config for --build and CTest
+export CMAKE_CONFIG_TYPE="$CMAKE_BUILD_TYPE"
+# Control the install prefix
+export CMAKE_INSTALL_PREFIX="${MONGOCRYPT_INSTALL_PREFIX-}"
+
+# Have CTest print test failure info to stderr
+export CTEST_OUTPUT_ON_FAILURE=1
+# Generate a compilation database for use by other tools
+export CMAKE_EXPORT_COMPILE_COMMANDS=1
+
+# Accumulate arguments that are passed to CMake
+cmake_args=(
+    # Set the source directory
+    "-H$LIBMONGOCRYPT_DIR"
+    # Set the build directory
+    "-B$BINARY_DIR"
+    # Set the build type. CMake 3.22 recognizes this via environment variable
+    -D CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"
+    # Set the install preifx. CMake 3.29 recognizes this via environment variable
+    -D CMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_PREFIX"
+)
+
+# shellcheck disable=SC2206
+cmake_args+=($ADDITIONAL_CMAKE_FLAGS)
+# shellcheck disable=SC2206
+cmake_args+=($LIBMONGOCRYPT_EXTRA_CMAKE_FLAGS)
+
 : "${CONFIGURE_ONLY:=}"
-: "${LIBMONGOCRYPT_BUILD_TYPE:=RelWithDebInfo}"
 
 if [ "$OS_NAME" = "windows" ]; then
     # Enable exception handling for MSVC
     CXXFLAGS="${CXXFLAGS-} -EHsc"
 fi
 
-# Have CTest print test failure info to stderr
-export CTEST_OUTPUT_ON_FAILURE=1
-
 if [ "$PPA_BUILD_ONLY" ]; then
     # Clean-up from previous build iteration
     rm -rf -- "$LIBMONGOCRYPT_DIR"/cmake-build* "$MONGOCRYPT_INSTALL_PREFIX"
-    ADDITIONAL_CMAKE_FLAGS="$ADDITIONAL_CMAKE_FLAGS -DENABLE_BUILD_FOR_PPA=ON"
+    cmake_args+=(-DENABLE_BUILD_FOR_PPA=ON)
 fi
 
 for suffix in "dll" "dylib" "so"; do
     cand="$(abspath "$LIBMONGOCRYPT_DIR/../mongocrypt_v1.$suffix")"
     if test -f "$cand"; then
-        ADDITIONAL_CMAKE_FLAGS="$ADDITIONAL_CMAKE_FLAGS -DMONGOCRYPT_TESTING_CRYPT_SHARED_FILE=$cand"
+        cmake_args+=("-DMONGOCRYPT_TESTING_CRYPT_SHARED_FILE=$cand")
     fi
 done
 
-ADDITIONAL_CMAKE_FLAGS="$ADDITIONAL_CMAKE_FLAGS -DENABLE_MORE_WARNINGS_AS_ERRORS=ON"
+cmake_args+=("-DENABLE_MORE_WARNINGS_AS_ERRORS=ON")
 
 build_dir="$LIBMONGOCRYPT_DIR/cmake-build"
-common_cmake_args=(
-    $ADDITIONAL_CMAKE_FLAGS
-    $LIBMONGOCRYPT_EXTRA_CMAKE_FLAGS
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-    -DCMAKE_BUILD_TYPE="$LIBMONGOCRYPT_BUILD_TYPE"
-    -H"$LIBMONGOCRYPT_DIR"
-    -B"$build_dir"
-)
 
 if test "${CMAKE_GENERATOR-}" = Ninja; then
     export NINJA_EXE
     : "${NINJA_EXE:="$build_dir/ninja$EXE_SUFFIX"}"
-    common_cmake_args+=(-DCMAKE_MAKE_PROGRAM="$NINJA_EXE")
+    cmake_args+=(-DCMAKE_MAKE_PROGRAM="$NINJA_EXE")
     bash "$EVG_DIR/ensure-ninja.sh"
 fi
 
 # Build and install libmongocrypt.
 run_cmake \
     -DCMAKE_INSTALL_PREFIX="$MONGOCRYPT_INSTALL_PREFIX" \
-    "${common_cmake_args[@]}"
+    "${cmake_args[@]}"
 
 if [ "$CONFIGURE_ONLY" ]; then
     echo "Only running cmake";
     exit 0;
 fi
 echo "Installing libmongocrypt"
-run_cmake --build "$build_dir" --target install --config "$LIBMONGOCRYPT_BUILD_TYPE"
-run_cmake --build "$build_dir" --target test-mongocrypt --config "$LIBMONGOCRYPT_BUILD_TYPE"
-run_cmake --build "$build_dir" --target test_kms_request --config "$LIBMONGOCRYPT_BUILD_TYPE"
-run_chdir "$build_dir" run_ctest -C "$LIBMONGOCRYPT_BUILD_TYPE"
+run_cmake --build "$build_dir" --target install
+run_cmake --build "$build_dir" --target test-mongocrypt
+run_cmake --build "$build_dir" --target test_kms_request
+run_chdir "$build_dir" run_ctest
 
 # MONGOCRYPT-372, ensure macOS universal builds contain both x86_64 and arm64 architectures.
 if test "${CMAKE_OSX_ARCHITECTURES-}" != ''; then
@@ -90,19 +116,19 @@ fi
 # Build and install libmongocrypt with no native crypto.
 run_cmake \
     -DDISABLE_NATIVE_CRYPTO=ON \
-    -DCMAKE_INSTALL_PREFIX="$MONGOCRYPT_INSTALL_PREFIX/nocrypto" \
-    "${common_cmake_args[@]}"
+    "${cmake_args[@]}" \
+    -DCMAKE_INSTALL_PREFIX="$MONGOCRYPT_INSTALL_PREFIX/nocrypto"
 
-run_cmake --build "$build_dir" --target install --config "$LIBMONGOCRYPT_BUILD_TYPE"
-run_cmake --build "$build_dir" --target test-mongocrypt --config "$LIBMONGOCRYPT_BUILD_TYPE"
-run_chdir "$build_dir" run_ctest -C "$LIBMONGOCRYPT_BUILD_TYPE"
+run_cmake --build "$build_dir" --target install
+run_cmake --build "$build_dir" --target test-mongocrypt
+run_chdir "$build_dir" run_ctest
 
 # Build and install libmongocrypt without statically linking libbson
 run_cmake \
     -UDISABLE_NATIVE_CRYPTO \
     -DUSE_SHARED_LIBBSON=ON \
-    -DCMAKE_INSTALL_PREFIX="$MONGOCRYPT_INSTALL_PREFIX/sharedbson" \
-    "${common_cmake_args[@]}"
+    "${cmake_args[@]}" \
+    -DCMAKE_INSTALL_PREFIX="$MONGOCRYPT_INSTALL_PREFIX/sharedbson"
 
-run_cmake --build "$build_dir" --target install  --config "$LIBMONGOCRYPT_BUILD_TYPE"
-run_chdir "$build_dir" run_ctest -C "$LIBMONGOCRYPT_BUILD_TYPE"
+run_cmake --build "$build_dir" --target install
+run_chdir "$build_dir" run_ctest
